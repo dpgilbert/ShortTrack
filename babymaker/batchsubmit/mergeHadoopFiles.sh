6a5b55925ed84afd60aeefd61fe06a2e92c68827
olivito <dominick.olivito@gmail.com>
2014-08-07 04:31:35 -0700

    updated scripts for merging

--- babymaker/batchsubmit/mergeHadoopFiles.sh
+++ babymaker/batchsubmit/mergeHadoopFiles.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-HADOOPDIR=/hadoop/cms/store/user/$USERNAME/babies/$1
-rm unmerged/*.root
+HADOOPDIR=/hadoop/cms/store/user/$USER/mt2babies/$1
+
+if [ ! -d "merged" ]; then
+    echo "creating merged directory"
+    mkdir merged
+fi
+#rm unmerged/*.root
 
 echo "
 void mergeHadoopFiles() {
   gSystem->Load(\"../libMiniFWLite.so\");
-  TChain *chain = new TChain(\"t\");
+  TChain *chain = new TChain(\"treeProducerSusyFullHad\");
   chain->SetMaxTreeSize(5000000000LL); //default is 100000000000LL
 " > mergeHadoopFiles.C
 
