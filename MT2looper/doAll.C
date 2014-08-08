3af9d6aeb73aba9a29ff19ae0c72e2a83ac0008b
gzevi <giovanni.zevidellaporta@gmail.com>
2014-08-08 03:03:11 -0700

    reads a ttbar baby

--- MT2looper/doAll.C
+++ MT2looper/doAll.C
@@ -6,14 +6,7 @@
 
   TChain *ch = new TChain("treeProducerSusyFullHad"); 
  
-  //CMS3 ntuples made from  
-  // /hadoop/cms/store/user/dalfonso/MINIAOD/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_PU20bx25.1.root
-  // /hadoop/cms/store/user/dalfonso/MINIAOD/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_PU20bx25.2.root 
-  //ch->Add("/nfs-3/userdata/jgran/MT2Sync/ntuple1.root");
-  //ch->Add("/nfs-3/userdata/jgran/MT2Sync/ntuple2.root");
-  //  ch->Add("/home/users/gzevi/miniAOD/CMSSW_7_0_6/src/CMS2/NtupleMaker/test/MT2ntuple1000.root");
-  ch->Add("sntMT2Baby.root");
-  //ch->Add("/home/users/jgran/CMSSW_7_0_6_patch1/src/CMS2/NtupleMaker/ntuple_first1000.root");
+  ch->Add("/nfs-6/userdata/mt2/V00-00-01/ttall_msdecays.root");
 
   MT2Looper *looper = new MT2Looper();
   looper->loop(ch, "plots"); 
