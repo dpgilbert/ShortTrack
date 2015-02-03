f2245de6555bffb34841409345f67820eafbbca8
olivito <dominick.olivito@gmail.com>
2015-02-03 11:12:20 +0100

    adding cms label macro

--- scripts/plotMaker.C
+++ scripts/plotMaker.C
@@ -15,6 +15,18 @@
 
 using namespace std;
 
+// -- for CMS_lumi label
+
+const int iPeriod = 4; // 13 tev
+
+// iPos drives the position of the CMS logo in the plot
+// iPos=11 : top-left, left-aligned
+// iPos=33 : top-right, right-aligned
+// iPos=22 : center, centered
+// mode generally : 
+//   iPos = 10*(alignement 1/2/3) + position (1/2/3 = left/center/right)
+const int iPos = 11; 
+
 //_______________________________________________________________________________
 int getColor(const string& sample) {
   if (sample.find("ttbar") != string::npos) return kBlue;
@@ -40,6 +52,7 @@ string getLegendName(const string& sample) {
   if (sample.find("T1bbbb_1000_900") != string::npos) return "T1bbbb 1000, 900";
   if (sample.find("T1bbbb_1500_100") != string::npos) return "T1bbbb 1500, 100";
   if (sample.find("T1qqqq_1400_100") != string::npos) return "T1qqqq 1400, 100";
+  if (sample.find("T1qqqq_1000_800") != string::npos) return "T1qqqq 1000, 800";
   if (sample.find("T2tt_425_325") != string::npos) return "T2tt 425, 325";
   if (sample.find("T2tt_500_325") != string::npos) return "T2tt 500, 325";
   if (sample.find("T2tt_650_325") != string::npos) return "T2tt 650, 325";
@@ -72,6 +85,7 @@ string getTableName(const string& sample) {
   if (sample.find("T1bbbb_1000_900") != string::npos) return "T1bbbb 1000, 900";
   if (sample.find("T1bbbb_1500_100") != string::npos) return "T1bbbb 1500, 100";
   if (sample.find("T1qqqq_1400_100") != string::npos) return "T1qqqq 1400, 100";
+  if (sample.find("T1qqqq_1000_800") != string::npos) return "T1qqqq 1000, 800";
   if (sample.find("T2tt_425_325") != string::npos) return "T2tt 425, 325";
   if (sample.find("T2tt_500_325") != string::npos) return "T2tt 500, 325";
   if (sample.find("T2tt_650_325") != string::npos) return "T2tt 650, 325";
@@ -106,7 +120,7 @@ string getRegionName(const string& dir) {
 }
 
 //_______________________________________________________________________________
-TCanvas* makePlot( const vector<TFile*>& samples , const vector<string>& names , const string& histdir , const string& histname , const string& xtitle , const string& ytitle , float xmin , float xmax , int rebin = 1 , bool logplot = true, bool printplot = false ) {
+TCanvas* makePlot( const vector<TFile*>& samples , const vector<string>& names , const string& histdir , const string& histname , const string& xtitle , const string& ytitle , float xmin , float xmax , int rebin = 1 , bool logplot = true, bool printplot = false, float scalesig = -1. ) {
 
   cout << "-- plotting histdir: " << histdir << ", histname: " << histname << endl;
 
@@ -114,13 +128,16 @@ TCanvas* makePlot( const vector<TFile*>& samples , const vector<string>& names ,
   TCanvas* can = new TCanvas(canvas_name,canvas_name);
   can->cd();
   if (logplot) can->SetLogy();
+  gPad->SetRightMargin(0.05);
+  gPad->Modified();
 
   THStack* t = new THStack(Form("stack_%s_%s",histdir.c_str(),histname.c_str()),Form("stack_%s_%s",histdir.c_str(),histname.c_str()));
   TH1D* h_bgtot = 0;
 
-  TLegend* leg = new TLegend(0.7,0.6,0.9,0.85);
+  TLegend* leg = new TLegend(0.55,0.6,0.85,0.92);
   leg->SetFillColor(0);
   leg->SetBorderSize(0);
+  leg->SetTextSize(0.032);
 
   // to make legend
   vector<TH1D*> bg_hists;
@@ -160,7 +177,9 @@ TCanvas* makePlot( const vector<TFile*>& samples , const vector<string>& names ,
 
   TH2F* h_axes = new TH2F(Form("%s_%s_axes",histname.c_str(),histdir.c_str()),"",100,xmin,xmax,100,ymin,ymax);
   h_axes->GetXaxis()->SetTitle(xtitle.c_str());
+  h_axes->GetXaxis()->SetLabelSize(0.04);
   h_axes->GetYaxis()->SetTitle(ytitle.c_str());
+  h_axes->GetYaxis()->SetLabelSize(0.04);
   h_axes->Draw();
 
   t->Draw("hist same");
@@ -177,13 +196,18 @@ TCanvas* makePlot( const vector<TFile*>& samples , const vector<string>& names ,
     h->SetLineColor(getColor(names.at(i)));
     h->SetLineWidth(2);
     if (rebin > 1) h->Rebin(rebin);
+    if (scalesig > 0.) h->Scale(scalesig);
     h->Draw("hist same");
-    leg->AddEntry(h,getLegendName(names.at(i)).c_str(),"l");
+    TString legend_name = getLegendName(names.at(i));
+    if (scalesig > 0.) legend_name += Form(" x %.0f",scalesig);
+    leg->AddEntry(h,legend_name,"l");
   }
-
 
   leg->Draw();
   h_axes->Draw("axissame");
+
+  CMS_lumi( can, iPeriod, iPos );
+
   gPad->Modified();
 
   if( printplot ) {
@@ -314,7 +338,12 @@ void printTable( vector<TFile*> samples , vector<string> names , vector<string> 
 //_______________________________________________________________________________
 void plotMaker(){
 
-  string input_dir = "/home/olivito/cms3/MT2Analysis/MT2looper/output/V00-00-07_sel2015LowLumi/";
+  gROOT->LoadMacro("CMS_lumi.C");
+  cmsText = "CMS Simulation";
+  writeExtraText = false;
+  lumi_13TeV = "4 fb^{-1}";
+
+  string input_dir = "/home/olivito/cms3/MT2Analysis/MT2looper/output/V00-00-08_4fb/";
 
   // ----------------------------------------
   //  samples definition
@@ -325,13 +354,14 @@ void plotMaker(){
   TFile* f_ttbar = new TFile(Form("%s/ttall_msdecays.root",input_dir.c_str()));
   TFile* f_zinv = new TFile(Form("%s/zinv_ht.root",input_dir.c_str()));
   TFile* f_wjets = new TFile(Form("%s/wjets_ht.root",input_dir.c_str()));
-  TFile* f_qcd = new TFile(Form("%s/qcd_pt.root",input_dir.c_str()));
+  TFile* f_qcd = new TFile(Form("%s/qcd_ht.root",input_dir.c_str()));
 
   // TFile* f_T1tttt_1500_100 = new TFile(Form("%s/T1tttt_1500_100.root",input_dir.c_str()));
   // TFile* f_T1tttt_1200_800 = new TFile(Form("%s/T1tttt_1200_800.root",input_dir.c_str()));
   // TFile* f_T1bbbb_1000_900 = new TFile(Form("%s/T1bbbb_1000_900.root",input_dir.c_str()));
   // TFile* f_T1bbbb_1500_100 = new TFile(Form("%s/T1bbbb_1500_100.root",input_dir.c_str()));
   // TFile* f_T1qqqq_1400_100 = new TFile(Form("%s/T1qqqq_1400_100.root",input_dir.c_str()));
+  // TFile* f_T1qqqq_1000_800 = new TFile(Form("%s/T1qqqq_1000_800.root",input_dir.c_str()));
 
   // TFile* f_T2tt_425_325 = new TFile(Form("%s/T2tt_425_325.root",input_dir.c_str()));
   // TFile* f_T2tt_500_325 = new TFile(Form("%s/T2tt_500_325.root",input_dir.c_str()));
@@ -363,14 +393,15 @@ void plotMaker(){
 
   // samples.push_back(f_T1tttt_1500_100); names.push_back("sig_T1tttt_1500_100");
   // samples.push_back(f_T1tttt_1200_800); names.push_back("sig_T1tttt_1200_800");
-  // samples.push_back(f_T1bbbb_1000_900); names.push_back("sig_T1bbbb_1000_900");
   // samples.push_back(f_T1bbbb_1500_100); names.push_back("sig_T1bbbb_1500_100");
+  // samples.push_back(f_T1bbbb_1000_900); names.push_back("sig_T1bbbb_1000_900");
   // samples.push_back(f_T1qqqq_1400_100); names.push_back("sig_T1qqqq_1400_100");
+  // samples.push_back(f_T1qqqq_1000_800); names.push_back("sig_T1qqqq_1000_800");
 
-  // samples.push_back(f_T2tt_425_325); names.push_back("sig_T2tt_425_325");
-  // samples.push_back(f_T2tt_500_325); names.push_back("sig_T2tt_500_325");
-  // samples.push_back(f_T2tt_650_325); names.push_back("sig_T2tt_650_325");
   // samples.push_back(f_T2tt_850_100); names.push_back("sig_T2tt_850_100");
+  // samples.push_back(f_T2tt_650_325); names.push_back("sig_T2tt_650_325");
+  // samples.push_back(f_T2tt_500_325); names.push_back("sig_T2tt_500_325");
+  // samples.push_back(f_T2tt_425_325); names.push_back("sig_T2tt_425_325");
 
   // samples.push_back(f_T2bb_900_100); names.push_back("sig_T2bb_900_100");
   // samples.push_back(f_T2bb_600_580); names.push_back("sig_T2bb_600_580");
@@ -391,65 +422,162 @@ void plotMaker(){
   //  plots definitions
   // ----------------------------------------
 
+  float scalesig = 50.;
   bool printplots = false;
 
-  makePlot( samples , names , "" , "h_SignalRegion"  , "Signal Region" , "Events" , 0 , 100 , 1 , true, printplots );
-  makePlot( samples , names , "nocut" , "h_ht"  , "H_{T} [GeV]" , "Events / 25 GeV" , 0 , 2000 , 1 , true, printplots );
-  makePlot( samples , names , "nocut" , "h_mt2" , "M_{T2} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
-  makePlot( samples , names , "nocut" , "h_met"  , "E_{T}^{miss} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
-  makePlot( samples , names , "nocut" , "h_nlepveto" , "N(leptons)" , "Events" , 0 , 10 , 1 , false, printplots );
-  makePlot( samples , names , "nocut" , "h_nJet40" , "N(jets)" , "Events" , 0 , 15 , 1 , false, printplots );
-  makePlot( samples , names , "nocut" , "h_nBJet40" , "N(b jets)" , "Events" , 0 , 6 , 1 , false, printplots );
+  // makePlot( samples , names , "" , "h_SignalRegion"  , "Signal Region" , "Events" , 0 , 100 , 1 , true, printplots );
+  // makePlot( samples , names , "nocut" , "h_ht"  , "H_{T} [GeV]" , "Events / 25 GeV" , 0 , 2000 , 1 , true, printplots );
+  // makePlot( samples , names , "nocut" , "h_mt2" , "M_{T2} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
+  // makePlot( samples , names , "nocut" , "h_met"  , "E_{T}^{miss} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
+  // makePlot( samples , names , "nocut" , "h_nlepveto" , "N(leptons)" , "Events" , 0 , 10 , 1 , false, printplots );
+  // makePlot( samples , names , "nocut" , "h_nJet40" , "N(jets)" , "Events" , 0 , 15 , 1 , false, printplots );
+  // makePlot( samples , names , "nocut" , "h_nBJet40" , "N(b jets)" , "Events" , 0 , 6 , 1 , false, printplots );
+  // makePlot( samples , names , "nocut" , "h_minMTBMet"  , "min M_{T}(b,MET) [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
+
+  makePlot( samples , names , "srbase" , "h_ht"  , "H_{T} [GeV]" , "Events / 25 GeV" , 0 , 2000 , 1 , true, printplots, scalesig );
+  makePlot( samples , names , "srbase" , "h_mt2" , "M_{T2} [GeV]" , "Events / 10 GeV" , 0 , 1000 , 1 , true, printplots, scalesig );
+  makePlot( samples , names , "srbase" , "h_nJet40" , "N(jets)" , "Events" , 0 , 15 , 1 , false, printplots, scalesig );
+  makePlot( samples , names , "srbase" , "h_nBJet40" , "N(b jets)" , "Events" , 0 , 6 , 1 , false, printplots, scalesig );
+  makePlot( samples , names , "srbase" , "h_minMTBMet"  , "min M_{T}(b,MET) [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , false, printplots, scalesig );
+
+  // makePlot( samples , names , "crslbase" , "h_ht"  , "H_{T} [GeV]" , "Events / 25 GeV" , 0 , 2000 , 1 , true, printplots );
+  // makePlot( samples , names , "crslbase" , "h_mt2" , "M_{T2} [GeV]" , "Events / 10 GeV" , 0 , 1000 , 1 , true, printplots );
+  // makePlot( samples , names , "crslbase" , "h_met"  , "E_{T}^{miss} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
+  // makePlot( samples , names , "crslbase" , "h_nJet40" , "N(jets)" , "Events" , 0 , 15 , 1 , false, printplots );
+  // makePlot( samples , names , "crslbase" , "h_nBJet40" , "N(b jets)" , "Events" , 0 , 6 , 1 , false, printplots );
+
+  // makePlot( samples , names , "crslw" , "h_ht"  , "H_{T} [GeV]" , "Events / 25 GeV" , 0 , 2000 , 1 , true, printplots );
+  // makePlot( samples , names , "crslw" , "h_mt2" , "M_{T2} [GeV]" , "Events / 10 GeV" , 0 , 1000 , 1 , true, printplots );
+  // makePlot( samples , names , "crslw" , "h_met"  , "E_{T}^{miss} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
+  // makePlot( samples , names , "crslw" , "h_nJet40" , "N(jets)" , "Events" , 0 , 15 , 1 , false, printplots );
+  // makePlot( samples , names , "crslw" , "h_nBJet40" , "N(b jets)" , "Events" , 0 , 6 , 1 , false, printplots );
+
+  // makePlot( samples , names , "crsltt" , "h_ht"  , "H_{T} [GeV]" , "Events / 25 GeV" , 0 , 2000 , 1 , true, printplots );
+  // makePlot( samples , names , "crsltt" , "h_mt2" , "M_{T2} [GeV]" , "Events / 10 GeV" , 0 , 1000 , 1 , true, printplots );
+  // makePlot( samples , names , "crsltt" , "h_met"  , "E_{T}^{miss} [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
+  // makePlot( samples , names , "crsltt" , "h_nJet40" , "N(jets)" , "Events" , 0 , 15 , 1 , false, printplots );
+  // makePlot( samples , names , "crsltt" , "h_nBJet40" , "N(b jets)" , "Events" , 0 , 6 , 1 , false, printplots );
+  // makePlot( samples , names , "crsltt" , "h_minMTBMet"  , "min M_{T}(b,MET) [GeV]" , "Events / 10 GeV" , 0 , 800 , 1 , true, printplots );
 
   // ----------------------------------------
   //  tables definitions
   // ----------------------------------------
 
-  vector<string> dirs;
-  dirs.push_back("nocut");
+  //  vector<string> dirs;
+  // //  dirs.push_back("nocut");
+  // dirs.push_back("crslbase");
+  // dirs.push_back("crslelbase");
+  // dirs.push_back("crslmubase");
+  // dirs.push_back("crslhadbase");
+  // dirs.push_back("crslw");
+  // dirs.push_back("crsltt");
 
-  printTable(samples, names, dirs);
+  // printTable(samples, names, dirs);
 
-  vector<string> dirsL;
-  dirsL.push_back("sr1L");
-  dirsL.push_back("sr2L");
-  dirsL.push_back("sr3L");
-  dirsL.push_back("sr4L");
-  dirsL.push_back("sr5L");
-  dirsL.push_back("sr6L");
-  dirsL.push_back("sr7L");
-  dirsL.push_back("sr8L");
-  dirsL.push_back("sr9L");
-  dirsL.push_back("sr10L");
+  // dirs.clear();
+  // dirs.push_back("srbase");
+  // dirs.push_back("srL");
+  // dirs.push_back("srM");
+  // dirs.push_back("srH");
 
-  vector<string> dirsM;
-  dirsM.push_back("sr1M");
-  dirsM.push_back("sr2M");
-  dirsM.push_back("sr3M");
-  dirsM.push_back("sr4M");
-  dirsM.push_back("sr5M");
-  dirsM.push_back("sr6M");
-  dirsM.push_back("sr7M");
-  dirsM.push_back("sr8M");
-  dirsM.push_back("sr9M");
-  dirsM.push_back("sr10M");
+  // printTable(samples, names, dirs);
 
-  vector<string> dirsH;
-  dirsH.push_back("sr1H");
-  dirsH.push_back("sr2H");
-  dirsH.push_back("sr3H");
-  dirsH.push_back("sr4H");
-  dirsH.push_back("sr5H");
-  dirsH.push_back("sr6H");
-  dirsH.push_back("sr7H");
-  dirsH.push_back("sr8H");
-  dirsH.push_back("sr9H");
-  dirsH.push_back("sr10H");
+  // dirs.clear();
+  // dirs.push_back("crslelw");
+  // dirs.push_back("crsleltt");
+  // dirs.push_back("crslmuw");
+  // dirs.push_back("crslmutt");
+  // dirs.push_back("crslhadw");
+  // dirs.push_back("crslhadtt");
 
-  // inclusive in mt2
-  printTable(samples, names, dirsL);
-  printTable(samples, names, dirsM);
-  printTable(samples, names, dirsH);
+  // printTable(samples, names, dirs);
+
+  // vector<string> dirsL;
+  // dirsL.push_back("sr1L");
+  // dirsL.push_back("sr2L");
+  // dirsL.push_back("sr3L");
+  // dirsL.push_back("sr4L");
+  // dirsL.push_back("sr5L");
+  // printTable(samples, names, dirsL);
+
+  // dirsL.clear();
+  // dirsL.push_back("sr6L");
+  // dirsL.push_back("sr7L");
+  // dirsL.push_back("sr8L");
+  // dirsL.push_back("sr9L");
+  // dirsL.push_back("sr10L");
+  // printTable(samples, names, dirsL);
+
+  // vector<string> dirsM;
+  // dirsM.push_back("sr1M");
+  // dirsM.push_back("sr2M");
+  // dirsM.push_back("sr3M");
+  // dirsM.push_back("sr4M");
+  // dirsM.push_back("sr5M");
+  // printTable(samples, names, dirsM);
+
+  // dirsM.clear();
+  // dirsM.push_back("sr6M");
+  // dirsM.push_back("sr7M");
+  // dirsM.push_back("sr8M");
+  // dirsM.push_back("sr9M");
+  // dirsM.push_back("sr10M");
+  // printTable(samples, names, dirsM);
+
+  // vector<string> dirsH;
+  // dirsH.push_back("sr1H");
+  // dirsH.push_back("sr2H");
+  // dirsH.push_back("sr3H");
+  // dirsH.push_back("sr4H");
+  // dirsH.push_back("sr5H");
+  // printTable(samples, names, dirsH);
+
+  // dirsH.clear();
+  // dirsH.push_back("sr6H");
+  // dirsH.push_back("sr7H");
+  // dirsH.push_back("sr8H");
+  // dirsH.push_back("sr9H");
+  // dirsH.push_back("sr10H");
+  // printTable(samples, names, dirsH);
+
+  // vector<string> dirsL;
+  // dirsL.push_back("crsl1L");
+  // dirsL.push_back("crsl2L");
+  // dirsL.push_back("crsl3L4L");
+  // dirsL.push_back("crsl5L");
+  // dirsL.push_back("crsl6L");
+  // dirsL.push_back("crsl7L8L");
+  // dirsL.push_back("crsl9L10L");
+
+  // vector<string> dirsM;
+  // dirsM.push_back("crsl1M");
+  // dirsM.push_back("crsl2M");
+  // dirsM.push_back("crsl3M4M");
+  // dirsM.push_back("crsl5M");
+  // dirsM.push_back("crsl6M");
+  // dirsM.push_back("crsl7M8M");
+  // dirsM.push_back("crsl9M10M");
+
+  // vector<string> dirsH;
+  // dirsH.push_back("crsl1H");
+  // dirsH.push_back("crsl2H");
+  // dirsH.push_back("crsl3H4H");
+  // dirsH.push_back("crsl5H");
+  // dirsH.push_back("crsl6H");
+  // dirsH.push_back("crsl7H8H");
+  // dirsH.push_back("crsl9H10H");
+
+
+  // printTable(samples, names, dirsH, 1);
+  // printTable(samples, names, dirsH, 2);
+  // printTable(samples, names, dirsH, 3);
+  // printTable(samples, names, dirsH, 4);
+  // printTable(samples, names, dirsH, 5);
+
+  // // inclusive in mt2
+  // printTable(samples, names, dirsL);
+  // printTable(samples, names, dirsM);
+  // printTable(samples, names, dirsH);
 
   // // mt2 bin 1
   // printTable(samples, names, dirsL, 1);
