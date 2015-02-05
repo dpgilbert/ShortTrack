d28f1c6d5c06873fadf5877e2d776eac5e8d6f53
jgran <jgran@physics.ucsb.edu>
2015-02-04 22:16:51 -0800

    initial commit for new branch

---
+++ MT2CORE/SR.h
@@ -0,0 +1,42 @@
+#include <string>
+#include <map>
+#include <vector>
+
+#include "TH1.h"
+
+
+class SR {
+
+  public:
+
+    SR();
+    ~SR();
+
+    void SetName(std::string sr_name);
+    void SetVar(std::string var_name, float lower_bound, float upper_bound);
+
+    std::string GetName();
+    float GetLowerBound(std::string var_name);
+    float GetUpperBound(std::string var_name);
+    int GetNumberOfVariables();
+    std::vector<std::string> GetListOfVariables();
+
+    bool PassesSelection(std::map<std::string, float> values);
+    void RemoveVar(std::string var_name);
+    void Clear();
+
+    //used for plotting
+    std::map<std::string, TH1D*> srHistMap;
+    std::map<std::string, TH1D*> crslHistMap;
+    std::map<std::string, TH1D*> crslmuHistMap;
+    std::map<std::string, TH1D*> crslelHistMap;
+    std::map<std::string, TH1D*> crslhadHistMap;
+    std::map<std::string, TH1D*> crgjHistMap;
+    std::map<std::string, TH1D*> crdyHistMap;
+
+  private:
+
+    std::string srName_;
+    std::map<std::string, std::pair<float, float> > bins_;
+
+};
