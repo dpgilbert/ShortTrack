3fb00163a4bff8b8993c768a18679d89ad07cccb
olivito <dominick.olivito@gmail.com>
2015-05-26 06:05:18 -0700

    put addTopSamples inside makeDataDrivenEstimates

--- scripts/makeDataDrivenEstimates.sh
+++ scripts/makeDataDrivenEstimates.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-INDIR=/home/users/gzevi/MT2/MT2Analysis/MT2looper/output/V00-00-11skim/
+INDIR=/home/users/olivito/MT2Analysis/MT2looper/output/V00-00-12_binsHI/
 THISDIR=`pwd`
 
+if [ ! -d "$INDIR" ]; then
+  echo "Input directory does not exist" 
+fi
 
+if [ -e "$INDIR/top.root" ]; then
+  echo "top.root already exists, will delete it and make a new one." 
+  rm $INDIR/top.root
+fi
+
+cd $INDIR
+echo "hadd top.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root"
+hadd top.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root
+cd $THISDIR
+
+#this script scales the HI and LOW boundary histograms by 1/numSamples since we don't want these hadd'ed
+echo "root -b -q rescaleBoundaryHists.C(${INDIR},5)"
+root -b -q "rescaleBoundaryHists.C(\"${INDIR}\",5)"
+
+# make the data driven background files
 echo "root -b -q lostlepMaker.C(${INDIR})"
 root -b -q "lostlepMaker.C(\"${INDIR}\")" > dataDrivenEstimates.log
 echo "root -b -q ZinvMaker.C(${INDIR})"
