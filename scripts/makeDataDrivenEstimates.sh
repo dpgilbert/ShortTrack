2d673c9fa22d63e928bad9ed3230c6c95963c8fc
olivito <dominick.olivito@gmail.com>
2015-05-26 10:12:54 -0700

    update lostlepMaker to have input slot for data. also compile bkg makers by default

--- scripts/makeDataDrivenEstimates.sh
+++ scripts/makeDataDrivenEstimates.sh
@@ -7,25 +7,27 @@ if [ ! -d "$INDIR" ]; then
   echo "Input directory does not exist" 
 fi
 
-if [ -e "$INDIR/top.root" ]; then
-  echo "top.root already exists, will delete it and make a new one." 
-  rm $INDIR/top.root
-fi
-
 cd $INDIR
-echo "hadd top.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root"
-hadd top.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root
+echo "hadd -f top.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root"
+hadd -f top.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root
+
+## make "lostlep" from sum of MC lostlep backgrounds (except QCD, to avoid spikes)
+echo "hadd -f lostlep.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root wjets_ht.root"
+hadd -f lostlep.root ttall_msdecays.root tth.root ttzjets.root ttwjets.root singletop.root wjets_ht.root
 cd $THISDIR
 
 #this script scales the HI and LOW boundary histograms by 1/numSamples since we don't want these hadd'ed
-echo "root -b -q rescaleBoundaryHists.C(${INDIR},5)"
-root -b -q "rescaleBoundaryHists.C(\"${INDIR}\",5)"
+echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/top.root,5)"
+root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/top.root\",5)"
+
+echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/lostlep.root,6)"
+root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/lostlep.root\",6)"
 
 # make the data driven background files
-echo "root -b -q lostlepMaker.C(${INDIR})"
-root -b -q "lostlepMaker.C(\"${INDIR}\")" > dataDrivenEstimates.log
-echo "root -b -q ZinvMaker.C(${INDIR})"
-root -b -q "ZinvMaker.C(\"${INDIR}\")" >> dataDrivenEstimates.log
+echo "root -b -q lostlepMaker.C+(${INDIR})"
+root -b -q "lostlepMaker.C+(\"${INDIR}\")" > dataDrivenEstimates.log
+echo "root -b -q ZinvMaker.C+(${INDIR})"
+root -b -q "ZinvMaker.C+(\"${INDIR}\")" >> dataDrivenEstimates.log
 cd $INDIR
 rm qcdplusgjet.root
 echo "hadd qcdplusgjet.root gjet_ht.root qcd_pt.root" 
