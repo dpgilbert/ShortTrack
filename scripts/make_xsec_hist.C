d2cb2b2d75d8dfb8518c7352c22870a54dbf3703
olivito <dominick.olivito@gmail.com>
2015-10-06 14:30:41 +0200

    script to make xsec hist from text file

---
+++ scripts/make_xsec_hist.C
@@ -0,0 +1,33 @@
+{
+
+  float sf = 1.;
+  TString sparticle = "gluino";
+
+  //float sf = 0.8;
+  //TString sparticle = "squark";
+
+  //float sf = 1.;
+  //TString sparticle = "stop";
+
+  TFile* f = new TFile("xsec_susy_13tev.root","RECREATE");
+  //TFile* f = new TFile("xsec_susy_13tev.root","UPDATE");
+  TH1F* h = new TH1F(Form("h_xsec_%s",sparticle.Data()),Form(";M(%s) [GeV];xsec [pb]",sparticle.Data()), 561, 197.5, 3002.5);
+  //TH1F* h = new TH1F(Form("h_xsec_%s",sparticle.Data()),Form(";M(%s) [GeV];xsec [pb]",sparticle.Data()), 381, 97.5, 2002.5);
+
+  int nlines = 0;
+  std::ifstream xsecifs(Form("xsec_%s_13tev.dat",sparticle.Data()));
+  float mass;
+  float xsec,err;
+  while (xsecifs >> mass >> xsec >> err) {
+    int bin = h->FindBin(mass);
+    h->SetBinContent(bin,xsec * sf);
+    h->SetBinError(bin,err/100. * xsec * sf);
+    ++nlines;
+  }
+  xsecifs.close();
+  f->Write();
+  f->Close();
+
+  std::cout << "read " << nlines << " masses" << std::endl;
+
+}
