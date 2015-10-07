78e7be7d4017b2cf2c272df65a7cf7a9993fd8a6
olivito <dominick.olivito@gmail.com>
2015-10-07 07:20:08 -0700

    updating for signal scans

--- limits/Asymptotic/doLimits.sh
+++ limits/Asymptotic/doLimits.sh
@@ -4,17 +4,18 @@
 
 cmsenv
 
-INDIR="/home/users/jgran/temp/new/MT2Analysis/scripts/cards_bjetpt20_nonjet"
-OUTDIR="test"
+INDIR=/home/users/olivito/mt2_74x_dev/MT2Analysis/scripts/cards_scantest
+OUTDIR=scantest
 
-#declare -a samples=("T1tttt_1500_100" "T1tttt_1200_800" "T1bbbb_1500_100" "T1bbbb_1000_900" "T1qqqq_1400_100" "T1qqqq_1000_800" "T2tt_850_100" "T2tt_650_325" "T2tt_500_325" "T2tt_425_325" "T2bb_900_100" "T2bb_600_580" "T2qq_1200_100" "T2qq_600_550")
-declare -a samples=("T1tttt_1500_100" "T1tttt_1200_800")
+#declare -a samples=(T1tttt_1500_100 T1tttt_1200_800 T1bbbb_1500_100 T1bbbb_1000_900 T1qqqq_1400_100 T1qqqq_1000_800 T2tt_850_100 T2tt_650_325 T2tt_500_325 T2tt_425_325 T2bb_900_100 T2bb_600_580 T2qq_1200_100 T2qq_600_550)
+#declare -a samples=(T1tttt_1500_100 T1tttt_1200_800)
+declare -a samples=(`cat $INDIR/points_T1bbbb.txt`)
 
 #need to combine cards from multiple signal regions if necessary
 for i in "${samples[@]}"
 do
-  if [ ! -e "$INDIR/card_all_$i.txt" ]; then
-    python combineCards.py "$INDIR/card_"*"_$i.txt" > "$INDIR/card_all_$i.txt"  
+  if [ ! -e "$INDIR/datacard_all_$i.txt" ]; then
+    python combineCards.py "$INDIR/datacard_"*"_$i.txt" > "$INDIR/datacard_all_$i.txt"  
   fi
 done
 
@@ -30,8 +31,8 @@ cd "$OUTDIR"
 
 for i in "${samples[@]}"
 do
-  echo "Running command: combine -M Asymptotic -n "$i" "$INDIR/card_all_$i.txt" > "log/limit_$i.txt" 2>&1"
-  combine -M Asymptotic -n "$i" "$INDIR/card_all_$i.txt" > "log/limit_$i.log" 2>&1
+  echo "Running command: combine -M Asymptotic -n "$i" "$INDIR/datacard_all_$i.txt" > "log/limit_$i.txt" 2>&1"
+  combine -M Asymptotic -n "$i" "$INDIR/datacard_all_$i.txt" > "log/limit_$i.log" 2>&1
   mv "higgsCombine"$i".Asymptotic.mH120.root" "limit_"$i".root"
   MODEL=$(echo "$i"|awk -F- 'split($1,a,"_")&&$0=a[1]') #because awk
   MASS1=$(echo "$i"|awk -F- 'split($1,a,"_")&&$0=a[2]')
