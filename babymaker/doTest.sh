1d2378798bd1512765abb5dd14c1fdfd38459a1d
olivito <dominick.olivito@gmail.com>
2015-10-16 08:53:20 -0700

    updating test files to miniAODv2

--- babymaker/doTest.sh
+++ babymaker/doTest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+make -j 8
+
 #./processBaby data_json_test /home/users/jgran/temp/74x_sync/CMSSW_7_4_3/src/CMS3/NtupleMaker/ntuple_data.root
 #./processBaby data_test  /nfs-7/userdata/jgran/74x_sync/ntuple_data.root
 #./processBaby data_json_fixrho  /home/users/jgran/temp/electronID/CMSSW_7_4_1_patch1/src/CMS3/NtupleMaker/ntuple_rereco.root
@@ -8,7 +10,8 @@
 #./processBaby qcd_test_noJEC_fixjetID_fixgamma /home/users/gzevi/ntupling/CMSSW_7_4_1_patch1/src/CMS3/NtupleMaker/ntupleQCD_Pt_1000to1400_10000.root
 
 ##### Run2 datasets for sync - note that these do NOT have the postprocessing vars
-./processBaby sync_ttbar_1k /nfs-7/userdata/olivito/mt2/run2/ntuple_ttbar_50ns_1k.root
-#./processBaby sync_ttbar_10k /nfs-7/userdata/olivito/mt2/run2/ntuple_ttbar_50ns_10k.root
-#./processBaby sync_data_JetHT_1k /nfs-7/userdata/olivito/mt2/run2/ntuple_data_Run2015B_JetHT_1k.root
-#./processBaby sync_data_JetHT_10k /nfs-7/userdata/olivito/mt2/run2/ntuple_data_Run2015B_JetHT_10k.root
+./processBaby test_ttbar_1k /nfs-7/userdata/olivito/mt2/run2_25ns_MiniAODv2/ntuple_ttbar_25ns_1k.root
+#./processBaby test_ttbar_10k /nfs-7/userdata/olivito/mt2/run2_25ns_MiniAODv2/ntuple_ttbar_25ns_10k.root
+#./processBaby test_data_JetHT_1k /nfs-7/userdata/olivito/mt2/run2_25ns_MiniAODv2/ntuple_data_Run2015D_JetHT_PromptReco-v4_1k.root
+#./processBaby test_data_JetHT_10k /nfs-7/userdata/olivito/mt2/run2_25ns_MiniAODv2/ntuple_data_Run2015D_JetHT_PromptReco-v4_10k.root
+
