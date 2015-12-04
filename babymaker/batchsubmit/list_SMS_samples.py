bbf22ff2170d00e83db5f67d22f328f09bf6d7e0
olivito <dominick.olivito@gmail.com>
2015-12-04 07:01:31 -0800

    script to list SMS samples for batch running

---
+++ babymaker/batchsubmit/list_SMS_samples.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+from glob import glob
+
+#scan = 'T1bbbb'
+#scan = 'T1tttt'
+scan = 'T1qqqq'
+
+basedir = '/hadoop/cms/store/group/snt/run2_fastsim/'
+tag = 'V07-04-12'
+generic = '_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-FastAsympt25ns_74X_mcRun2_asymptotic_v2-v1'
+
+outf = open('commands_%s.txt'%(scan),'w')
+outfm = open('merge_%s.txt'%(scan),'w')
+
+for dir in glob('%s/SMS-%s*'%(basedir,scan)):
+    shortname = dir.replace(basedir,'').replace(generic,'').replace('SMS-','')
+    outf.write('./writeConfig.sh %s/%s/ ${TAG}_%s\n'%(dir,tag,shortname))
+    outfm.write('run %s\n'%(shortname))
+
+outf.close()
+outfm.close()
