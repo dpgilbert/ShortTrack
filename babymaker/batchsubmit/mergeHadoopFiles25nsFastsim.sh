862e2e4af3d9628e6d6cd338591fc73b8fc7de51
olivito <dominick.olivito@gmail.com>
2016-01-04 10:55:08 -0800

    updating sample lists

--- babymaker/batchsubmit/mergeHadoopFiles25nsFastsim.sh
+++ babymaker/batchsubmit/mergeHadoopFiles25nsFastsim.sh
@@ -5,7 +5,7 @@ function run () {
     nohup nice -n 19 root -b -q mergeHadoopFiles.C\(\"${HADOOPDIR}/${TAG}_$1/\",\"${OUTPUTDIR}/$1.root\"\) >& log_merge_${TAG}_$1.txt &
 }
 
-TAG=V00-01-09_25ns_fastsim
+TAG=V00-01-10_25ns_fastsim
 
 HADOOPDIR=/hadoop/cms/store/user/${USER}/mt2babies/
 OUTPUTDIR=/nfs-6/userdata/mt2/$TAG/
@@ -68,20 +68,26 @@ run T1bbbb_mGluino-975_mLSP-950
 # T1tttt
 #
 
+run T1tttt_mGluino-1000_mLSP-1to700
 run T1tttt_mGluino-1000to1050_mLSP-1to800
+run T1tttt_mGluino-1050_mLSP-50to775
 run T1tttt_mGluino-1100_mLSP-1to775
 run T1tttt_mGluino-1100to1125_mLSP-700to900
 run T1tttt_mGluino-1150_mLSP-1to800
 run T1tttt_mGluino-1150to1175_mLSP-750to925
+
 run T1tttt_mGluino-1175_mLSP-950
 run T1tttt_mGluino-1200_mLSP-1to825
 run T1tttt_mGluino-1200to1225_mLSP-800to1000
+run T1tttt_mGluino-1225to1250_mLSP-1to1025
+run T1tttt_mGluino-1250to1275_mLSP-700to1050
 run T1tttt_mGluino-1275_mLSP-900to975
 run T1tttt_mGluino-1300_mLSP-1to1075
 run T1tttt_mGluino-1300to1325_mLSP-700to1100
 
 run T1tttt_mGluino-1325to1350_mLSP-1to1125
 run T1tttt_mGluino-1350to1375_mLSP-50to1025
+run T1tttt_mGluino-1375_mLSP-950to1150
 run T1tttt_mGluino-1400_mLSP-1to1175
 run T1tttt_mGluino-1400to1425_mLSP-50to1100
 run T1tttt_mGluino-1425to1450_mLSP-1to1200
@@ -90,27 +96,33 @@ run T1tttt_mGluino-1475to1500_mLSP-1to1250
 run T1tttt_mGluino-1500to1525_mLSP-50to1125
 run T1tttt_mGluino-1525to1550_mLSP-1to1300
 
+run T1tttt_mGluino-1550to1575_mLSP-500to1175
 run T1tttt_mGluino-1600to1650_mLSP-1to1350
 run T1tttt_mGluino-1650to1700_mLSP-1to1400
 run T1tttt_mGluino-1700to1750_mLSP-1to1450
+run T1tttt_mGluino-1750_mLSP-50to1450
 run T1tttt_mGluino-1800to1850_mLSP-1to1450
 run T1tttt_mGluino-1850to1900_mLSP-1to1450
 run T1tttt_mGluino-1900to1950_mLSP-0to1450
 run T1tttt_mGluino-1950_mLSP-700to950
-run T1tttt_mGluino-625_mLSP-275to375
+run T1tttt_mGluino-600_mLSP-1to225
 
+run T1tttt_mGluino-600_mLSP-250to325
+run T1tttt_mGluino-600to625_mLSP-250to375
+run T1tttt_mGluino-625_mLSP-275to375
 run T1tttt_mGluino-625to650_mLSP-200to400
 run T1tttt_mGluino-650to675_mLSP-250to425
 run T1tttt_mGluino-675_mLSP-325to450
 run T1tttt_mGluino-700_mLSP-1to450
 run T1tttt_mGluino-700to750_mLSP-200to500
+
 run T1tttt_mGluino-750to775_mLSP-350to525
 run T1tttt_mGluino-775_mLSP-475to550
-
 run T1tttt_mGluino-800to825_mLSP-1to575
 run T1tttt_mGluino-825to850_mLSP-200to600
 run T1tttt_mGluino-850to875_mLSP-450to625
 run T1tttt_mGluino-875to900_mLSP-1to650
+run T1tttt_mGluino-900to950_mLSP-200to700
 run T1tttt_mGluino-950to975_mLSP-350to725
 run T1tttt_mGluino-975_mLSP-600to750
 
@@ -161,3 +173,18 @@ run T1qqqq_mGluino-825to875_mLSP-400to825
 run T1qqqq_mGluino-875to925_mLSP-1to850
 run T1qqqq_mGluino-925to975_mLSP-400to900
 run T1qqqq_mGluino-925to975_mLSP-950
+
+#
+# T2tt
+#
+
+run T2tt_mStop-100-125_mLSP-1to50
+run T2tt_mStop-150-175_mLSP-1to100
+run T2tt_mStop-200_mLSP-1to125
+run T2tt_mStop-225_mLSP-25to150
+run T2tt_mStop-250_mLSP-1to175
+run T2tt_mStop-275_mLSP-75to200
+run T2tt_mStop-300to375_mLSP-1to300
+run T2tt_mStop-400to475_mLSP-1to400
+run T2tt_mStop-500-525-550_mLSP-1to425-325to450-1to475
+run T2tt_mStop-600-950_mLSP-1to450
