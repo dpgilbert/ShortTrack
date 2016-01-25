db5e5c8402120b7813b928171d60e2c7d180b454
olivito <dominick.olivito@gmail.com>
2016-01-25 06:31:31 -0800

    fix sparm reading for T2bb, T2qq

--- babymaker/ScanChain.cc
+++ babymaker/ScanChain.cc
@@ -484,10 +484,24 @@ void babyMaker::ScanChain(TChain* chain, std::string baby_name, int bx, bool isF
             if (sparm_names().at(i).Contains("mLSP")) GenSusyMScan2 = sparm_values().at(i);
           }
         }
-	// T2 models
-        else if (evt_id >= 1100 && evt_id < 1200) {
+	// T2 models: T2tt and T2cc
+        else if ((evt_id >= 1100 && evt_id < 1110) || (evt_id >= 1130 && evt_id < 1140)) {
           for (unsigned int i=0; i<sparm_values().size(); ++i) {
             if (sparm_names().at(i).Contains("mStop")) GenSusyMScan1 = sparm_values().at(i);
+            if (sparm_names().at(i).Contains("mLSP")) GenSusyMScan2 = sparm_values().at(i);
+          }
+        }
+	// T2 models: T2qq
+        else if (evt_id >= 1110 && evt_id < 1120) {
+          for (unsigned int i=0; i<sparm_values().size(); ++i) {
+            if (sparm_names().at(i).Contains("mSquark")) GenSusyMScan1 = sparm_values().at(i);
+            if (sparm_names().at(i).Contains("mLSP")) GenSusyMScan2 = sparm_values().at(i);
+          }
+        }
+	// T2 models: T2bb
+        else if (evt_id >= 1120 && evt_id < 1130) {
+          for (unsigned int i=0; i<sparm_values().size(); ++i) {
+            if (sparm_names().at(i).Contains("mSbottom")) GenSusyMScan1 = sparm_values().at(i);
             if (sparm_names().at(i).Contains("mLSP")) GenSusyMScan2 = sparm_values().at(i);
           }
         }
