469932c0cfbaac62d93225b482616266868c9bdd
bmarsh@umail.ucsb.edu 6-10-2015 <bemarsh@uaf-7.t2.ucsd.edu>
2016-09-01 16:24:08 -0700

    modifications to condor submission scripts for computing limits

--- limits/SignalScan/condorExecutable.sh
+++ limits/SignalScan/condorExecutable.sh
@@ -11,17 +11,6 @@ echo "args: $@"
 #Tell us where we're running
 echo "host: `hostname`" 
 
-#Set environment
-export SCRAM_ARCH=slc6_amd64_gcc491
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-pushd /cvmfs/cms.cern.ch/slc6_amd64_gcc481/cms/cmssw/CMSSW_7_1_5/src/
-eval `scramv1 runtime -sh`
-echo "should be in cvmfs: $PWD"
-popd
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-export PATH=$PATH:.
-
 #untar tarball containing input files
 if [ -e job_input.tar.gz ]
 then
@@ -31,6 +20,22 @@ else
   exit 1
 fi
 
+mv cards*.tar.gz CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit/scripts
+
+#Set environment
+export SCRAM_ARCH=slc6_amd64_gcc491
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+# jumping through hoops to try to get this to work.....
+cd CMSSW_7_4_7/src/HiggsAnalysis/CombinedLimit
+eval `scramv1 runtime -sh`
+scramv1 b ProjectRename
+scramv1 b clean; scramv1 b
+eval `scramv1 runtime -sh`
+cd scripts
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+export PATH=$PATH:.
+
 if [ -e cards_$SAMPLE.tar.gz ]
 then
   tar -xzvf cards_$SAMPLE.tar.gz
@@ -39,9 +44,9 @@ else
   exit 1
 fi
 
-export PYTHONPATH="${PYTHONPATH}:${PWD}/HiggsAnalysis/CombinedLimit"
-echo "PYTHONPATH is set to:"
-echo $PYTHONPATH
+# export PYTHONPATH="${PYTHONPATH}:${PWD}/HiggsAnalysis/CombinedLimit"
+# echo "PYTHONPATH is set to:"
+# echo $PYTHONPATH
 
 which root
 
@@ -52,14 +57,14 @@ echo "running combineCards.py"
 python combineCards.py -S "datacard_"*"_$SAMPLE.txt" > "card_all_$SAMPLE.txt"  
 echo "ls -lrth after combineCards.py"
 ls -lrth
-echo "running text2workspace.py"
-./text2workspace.py card_all_$SAMPLE.txt -b -o $SAMPLE.root
-echo "ls -lrth after text2workspace.py"
-ls -lrth
+# echo "running text2workspace.py"
+# ./text2workspace.py card_all_$SAMPLE.txt -b -o $SAMPLE.root
+# echo "ls -lrth after text2workspace.py"
+# ls -lrth
 #echo "Running command: combine -M Asymptotic -n "$SAMPLE" "$SAMPLE.root --noFitAsimov" > "log_$SAMPLE.log" 2>&1"
 #combine -M Asymptotic -n "$SAMPLE" "$SAMPLE.root" --noFitAsimov > "log_$SAMPLE.log" 2>&1
-echo "Running command: combine -M Asymptotic -n "$SAMPLE" "$SAMPLE.root" > "log_$SAMPLE.log" 2>&1"
-combine -M Asymptotic -n "$SAMPLE" "$SAMPLE.root" > "log_$SAMPLE.log" 2>&1
+echo "Running command: combine -M Asymptotic -n "$SAMPLE" "$card_all_SAMPLE.txt" > "log_$SAMPLE.log" 2>&1"
+combine -M Asymptotic -n "$SAMPLE" "card_all_$SAMPLE.txt" 2>&1
 echo "ls -lrth after calculating the limit"
 ls -lrth
 mv "higgsCombine"$SAMPLE".Asymptotic.mH120.root" "limit_"$SAMPLE".root"
@@ -73,7 +78,7 @@ if [ ! -d "${OUTPUT_DIR}" ]; then
 fi
 
 #Copy the output
-lcg-cp -b -D srmv2 --vo cms --connect-timeout 2400 --verbose file://`pwd`/limit_$SAMPLE.root srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${OUTPUT_DIR}/limit_$SAMPLE.root
+gfal-copy -p -f -t 4200 --verbose file:`pwd`/limit_$SAMPLE.root srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${OUTPUT_DIR}/limit_$SAMPLE.root
 stageout_error=$?
 
 if [ $stageout_error != 0 ]
