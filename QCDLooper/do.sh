1d7d927a4f334e72bd962e912adda51e61064ebb
fgolf <frank.golf@gmail.com>
2016-12-09 04:11:50 -0800

    run on each era of data in parallel

--- QCDLooper/do.sh
+++ QCDLooper/do.sh
@@ -2,19 +2,28 @@
 
 make -j 8
 
-# INDIR=/nfs-6/userdata/mt2/rphi_skim_V00-08-04_MiniAODv2_nojson
-INDIR=/nfs-6/userdata/mt2/rphi_skim_V00-08-08_nojson
-# INDIR=/nfs-6/userdata/mt2/rphi_skim_V00-01-09_25ns
-OUTDIR=output/test
+OUTDIR=output/full2016
 
 LOGDIR=logs
 
-declare -a Samples=(qcd_ht ttsl ttdl singletop ttw ttz ttg dyjetsll_ht wjets_ht gjets_dr0p05_ht data_Run2016)
-# declare -a Samples=(qcd_ht)  
-# declare -a Samples=(data_Run2016)
-
 mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
+
+
+#most recent MC
+INDIR=/nfs-6/userdata/mt2/rphi_skim_V00-08-10_JECandRenorm
+
+declare -a Samples=(qcd_ht ttsl ttdl singletop ttw_mg_lo ttz_mg_lo ttg dyjetsll_ht wjets_ht gjets_dr0p05_ht zinv_ht)
+
+for SAMPLE in ${Samples[@]};
+  do echo ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
+  nohup ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
+done
+
+#most recent run2016H
+INDIR=/nfs-6/userdata/mt2/rphi_skim_V00-08-12_json_271036-284044_23Sep2016ReReco_36p26fb 
+
+declare -a Samples=(data_Run2016B.root data_Run2016C.root data_Run2016D.root data_Run2016E.root data_Run2016F.root data_Run2016G.root)
 
 for SAMPLE in ${Samples[@]};
   do echo ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
