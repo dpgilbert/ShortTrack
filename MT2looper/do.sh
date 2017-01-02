c9bd395a38a0d97d901076a79103d7453a719e74
fgolf <frank.golf@gmail.com>
2017-01-02 13:16:02 -0800

    fix merge conflict

--- MT2looper/do.sh
+++ MT2looper/do.sh
@@ -3,26 +3,25 @@
 make -j12
 if [[ $? != 0 ]]; then return $?; fi
 
-
-#INDIR=/nfs-6/userdata/mt2/V00-08-12_json_271036-284044_23Sep2016ReReco_36p26fb_skim_base_mt2gt200_ZinvV6
-INDIR=/nfs-6/userdata/mt2/V00-08-10_JECandRenorm_nojson_skim_base_mt2gt200_ZinvV6/
-OUTDIR=output/V00-08-09_16Dec16/
+OUTDIR=output/full2016
 LOGDIR=logs/
-
-#declare -a Samples=(data_Run2016B data_Run2016C data_Run2016D data_Run2016E data_Run2016F data_Run2016G data_Run2016H)
-declare -a Samples=(zinv_ht ttsl ttdl singletop qcd_ht ttw_mg_lo ttz_mg_lo ttg dyjetsll_ht gjets_dr0p05_ht wjets_ht100to200 wjets_ht200to400 wjets_ht400to600 wjets_ht600to800 wjets_ht800to1200 wjets_ht1200to2500 wjets_ht2500toInf ww )
-
-
-#declare -a Samples=(T1tttt_1500_100 T1tttt_1200_800 T1bbbb_1500_100 T1bbbb_1000_900 T1qqqq_1400_100 T1qqqq_1000_800 T2tt_850_100 T2tt_650_325 T2tt_500_325 T2tt_425_325 T2bb_900_100 T2bb_600_580 T2qq_1200_100 T2qq_600_550)
-#declare -a Samples=(T1tttt_1500_100 T1tttt_1200_800 T1bbbb_1500_100 T1bbbb_1000_900 T1qqqq_1400_100 T1qqqq_1000_800)
-#declare -a Samples=(T1bbbb)
-
-
-
 mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
+
+#most recent MC
+INDIR=/nfs-6/userdata/mt2/V00-08-10_JECandRenorm_nojson_skim_base_mt2gt200_ZinvV6/
+declare -a Samples=(ww ttsl ttdl singletop qcd_ht ttw_mg_lo ttz_mg_lo ttg dyjetsll_ht gjets_dr0p05_ht wjets_ht100to200 wjets_ht1200to2500 wjets_ht200to400 wjets_ht2500toInf wjets_ht400to600 wjets_ht600to800 wjets_ht800to1200 zinv_ht)
 
 for SAMPLE in ${Samples[@]};
   do echo ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
      nohup ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
+done
+
+#most recent run2016H and re-reco
+INDIR=/nfs-6/userdata/mt2/V00-08-12_json_271036-284044_23Sep2016ReReco_36p26fb_skim_base_mt2gt200_ZinvV6/
+declare -a Samples=(data_Run2016B data_Run2016C data_Run2016D data_Run2016E data_Run2016F data_Run2016G data_Run2016H)
+
+for SAMPLE in ${Samples[@]};
+  do echo ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
+  nohup ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
 done
