f08007f2f6441bd80fbb271cd3ac5afa90424eba
olivito <dominick.olivito@gmail.com>
2017-01-05 04:58:25 -0800

    updates for AN plots

--- scripts/trigeff/plot_trigeff_PFMETOR.C
+++ scripts/trigeff/plot_trigeff_PFMETOR.C
@@ -23,13 +23,13 @@ const int iPeriod = 4; // 13 tev
 //   iPos = 10*(alignement 1/2/3) + position (1/2/3 = left/center/right)
 const int iPos = 3;
 
-void plot_trigeff_PFMETOR (const TString& indir = "/nfs-6/userdata/mt2/V00-08-07_nojson/trigdenom_json_271036-276811/") {
+void plot_trigeff_PFMETOR (const TString& indir = "/nfs-6/userdata/mt2/V00-08-12_json_271036-284044_23Sep2016ReReco_36p26fb/trigdenom/") {
 
   cmsText = "CMS Preliminary";
   cmsTextSize = 0.5;
   lumiTextSize = 0.4;
   writeExtraText = false;
-  lumi_13TeV = "12.9 fb^{-1}";
+  lumi_13TeV = "36.5 fb^{-1}";
   //lumi_13TeV = "589 pb^{-1}";
   
   gStyle->SetPadTopMargin(0.08);
@@ -39,47 +39,62 @@ void plot_trigeff_PFMETOR (const TString& indir = "/nfs-6/userdata/mt2/V00-08-07
 
   TH1::SetDefaultSumw2();
 
-  TString suffix = "";
-  //  TString suffix = "_2j";
+  //  TString suffix = "";
+  TString suffix = "_36p5fb_jetpt450_1j";
   
-  // TChain* t_jetht = new TChain("mt2");
+  TChain* t_jetht = new TChain("mt2");
   // TChain* t_muon = new TChain("mt2");
   TChain* t_ele = new TChain("mt2");
 
-  // t_jetht->Add(Form("%s/*Run2016*JetHT*.root", indir.Data()));
+  t_jetht->Add(Form("%s/*Run2016*JetHT*.root", indir.Data()));
   // t_muon->Add(Form("%s/*Run2016*SingleMuon*.root", indir.Data()));
   t_ele->Add(Form("%s/*Run2016*SingleElectron*.root", indir.Data()));
 
   TFile* f_out = new TFile(Form("trigeff_OR_of_METtrigs%s.root",suffix.Data()),"RECREATE");
   
   TH1D* h_met_denom_ht800 = new TH1D("h_met_denom_ht800",";E_{T}^{miss} [GeV]",40,100,500);
-  TH1D* h_met_num_ht800 = (TH1D*) h_met_denom_ht800->Clone("h_met_num_ht800");
+  TH1D* h_metor_num_ht800 = (TH1D*) h_met_denom_ht800->Clone("h_metor_num_ht800");
+  TH1D* h_met120_num_ht800 = (TH1D*) h_met_denom_ht800->Clone("h_met120_num_ht800");
+  TH1D* h_htmet_num_ht800 = (TH1D*) h_met_denom_ht800->Clone("h_htmet_num_ht800");
   TH1D* h_met_denom_mu20 = (TH1D*) h_met_denom_ht800->Clone("h_met_denom_mu20");
   TH1D* h_met_num_mu20 = (TH1D*) h_met_denom_ht800->Clone("h_met_num_mu20");
   TH1D* h_met_denom_ele23 = (TH1D*) h_met_denom_ht800->Clone("h_met_denom_ele23");
-  TH1D* h_met_num_ele23 = (TH1D*) h_met_denom_ht800->Clone("h_met_num_ele23");
+  TH1D* h_metor_num_ele23 = (TH1D*) h_met_denom_ht800->Clone("h_metor_num_ele23");
+  TH1D* h_met120_num_ele23 = (TH1D*) h_met_denom_ht800->Clone("h_met120_num_ele23");
+  TH1D* h_htmet_num_ele23 = (TH1D*) h_met_denom_ht800->Clone("h_htmet_num_ele23");
 
   TCanvas* c = new TCanvas("c","c");
   c->SetGrid(1,1);
   c->cd();
 
-  //  TCut base = "nVert > 0 && nJet30 > 1 && Flag_CSCTightHalo2015Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && ht > 200.";
-  TCut base = "nVert > 0 && nJet30 > 1 && Flag_globalTightHalo2016Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && Flag_badMuonFilter && Flag_badChargedHadronFilter && ht > 200. && isGolden";
-  //TCut base = "nVert > 0 && nJet30 > 3 && Flag_CSCTightHaloFilter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHEIsoNoiseFilter";
-  TCut had = base + "nElectrons10+nMuons10==0 && ht > 1000. && deltaPhiMin > 0.3";
-  TCut ele = base + "nElectrons10 > 0 && abs(lep_pdgId[0]) == 11 && lep_pt[0] > 25. && lep_tightId[0] > 2 && lep_relIso03[0] < 0.1";
+  //  TCut base = "nVert > 0 && nJet30 > 1 && Flag_globalTightHalo2016Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && Flag_badMuonFilter && Flag_badChargedHadronFilter && ht > 250. && isGolden";
+  // TCut had = base + "nElectrons10+nMuons10==0 && ht > 1000. && deltaPhiMin > 0.3";
+  //  TCut base = "nVert > 0 && nJet30 > 1 && Flag_globalTightHalo2016Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && Flag_badMuonFilter && Flag_badChargedHadronFilter && ht > 250. && isGolden && deltaPhiMin < 0.3";
+  TCut base = "nVert > 0 && nJet30 == 1 && Flag_globalTightHalo2016Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && Flag_badMuonFilter && Flag_badChargedHadronFilter && jet1_pt > 450. && isGolden";
+  TCut ele = base + "nElectrons10 > 0 && abs(lep_pdgId[0]) == 11 && lep_pt[0] > 30. && lep_tightId[0] > 2 && lep_relIso03[0] < 0.1";
   TCut mu = base + "nMuons10 > 0 && abs(lep_pdgId[0]) == 13 && lep_pt[0] > 25. && lep_tightId[0] > 0 && lep_relIso03[0] < 0.1";
+  //  TCut had = base + "nElectrons10+nMuons10==0 && (HLT_PFHT475_Prescale || HLT_PFHT600_Prescale) && ht > 600. && deltaPhiMin < 0.3 && mt2 < 200";
+  TCut had = base + "nElectrons10+nMuons10==0 && (HLT_PFHT125_Prescale || HLT_PFHT200_Prescale || HLT_PFHT300_Prescale) && ht > 250 && ht < 575. && deltaPhiMin < 0.3 && mt2 < 200";
 
-  // t_jetht->Draw("met_pt>>h_met_denom_ht800",had+"HLT_PFHT800");
-  // t_jetht->Draw("met_pt>>h_met_num_ht800",had+"HLT_PFHT800 && HLT_PFMET100");
+  // t_jetht->Draw("met_pt>>h_met_denom_ht800",had);
+  // t_jetht->Draw("met_pt>>h_metor_num_ht800",had+"(HLT_PFMET120_PFMHT120 || HLT_PFHT300_PFMET110)");
+  // t_jetht->Draw("met_pt>>h_met120_num_ht800",had+"(HLT_PFMET120_PFMHT120)");
+  // t_jetht->Draw("met_pt>>h_htmet_num_ht800",had+"(HLT_PFHT300_PFMET110)");
 
   // t_muon->Draw("met_pt>>h_met_denom_mu20",mu+"HLT_SingleMu");
   // t_muon->Draw("met_pt>>h_met_num_mu20",mu+"HLT_SingleMu && HLT_PFMET100_PFMHT100");
 
-  t_ele->Draw("met_pt>>h_met_denom_ele23",ele+"ht > 500. && HLT_SingleEl");
-  t_ele->Draw("met_pt>>h_met_num_ele23",ele+"ht > 500. && HLT_SingleEl && (HLT_PFMET100_PFMHT100 || HLT_PFHT300_PFMET100)");
+  // t_ele->Draw("met_pt>>h_met_denom_ele23",ele+"ht > 450. && HLT_SingleEl");
+  // t_ele->Draw("met_pt>>h_metor_num_ele23",ele+"ht > 450. && HLT_SingleEl && (HLT_PFMET120_PFMHT120 || HLT_PFHT300_PFMET110)");
+  // t_ele->Draw("met_pt>>h_met120_num_ele23",ele+"ht > 450. && HLT_SingleEl && (HLT_PFMET120_PFMHT120)");
+  // t_ele->Draw("met_pt>>h_htmet_num_ele23",ele+"ht > 450. && HLT_SingleEl && (HLT_PFHT300_PFMET110)");
 
-  TH2F* h_axis = new TH2F("h_axis",";E_{T}^{miss} [GeV];Eff of paths ORed",40,100,500,20,0,1);
+  t_ele->Draw("met_pt>>h_met_denom_ele23",ele+"jet1_pt > 450. && HLT_SingleEl");
+  t_ele->Draw("met_pt>>h_metor_num_ele23",ele+"jet1_pt > 450. && HLT_SingleEl && (HLT_PFMET120_PFMHT120 || HLT_PFHT300_PFMET110)");
+  t_ele->Draw("met_pt>>h_met120_num_ele23",ele+"jet1_pt > 450. && HLT_SingleEl && (HLT_PFMET120_PFMHT120)");
+  t_ele->Draw("met_pt>>h_htmet_num_ele23",ele+"jet1_pt > 450. && HLT_SingleEl && (HLT_PFHT300_PFMET110)");
+
+  TH2F* h_axis = new TH2F("h_axis",";E_{T}^{miss} [GeV];Efficiency",40,100,500,20,0,1);
   h_axis->GetYaxis()->SetTitleOffset(0.98);
   h_axis->Draw();
   
@@ -91,28 +106,57 @@ void plot_trigeff_PFMETOR (const TString& indir = "/nfs-6/userdata/mt2/V00-08-07
   // h_met_eff_mu20->SetLineColor(kBlue);
   // h_met_eff_mu20->SetMarkerColor(kBlue);
 
-  TEfficiency* h_met_eff_ele23 = new TEfficiency(*h_met_num_ele23, *h_met_denom_ele23);
-  h_met_eff_ele23->SetLineColor(kGreen+2);
-  h_met_eff_ele23->SetMarkerColor(kGreen+2);
+  // TEfficiency* h_metor_eff_ht800 = new TEfficiency(*h_metor_num_ht800, *h_met_denom_ht800);
+  // h_metor_eff_ht800->SetLineColor(kBlue);
+  // h_metor_eff_ht800->SetMarkerColor(kBlue);
 
-  int bin_met200 = h_met_denom_ele23->FindBin(200.);
-  float num_met200 = h_met_num_ele23->Integral(bin_met200,-1);
-  float denom_met200 = h_met_denom_ele23->Integral(bin_met200,-1);
-  float eff_met200_ele23 = num_met200/denom_met200;
-  float eff_UP_met200_ele23 = TEfficiency::ClopperPearson(denom_met200, num_met200, 0.68, 1);
-  float eff_DN_met200_ele23 = TEfficiency::ClopperPearson(denom_met200, num_met200, 0.68, 0);
+  // TEfficiency* h_met120_eff_ht800 = new TEfficiency(*h_met120_num_ht800, *h_met_denom_ht800);
+  // h_met120_eff_ht800->SetLineColor(kRed);
+  // h_met120_eff_ht800->SetMarkerColor(kRed);
 
-  std::cout << "efficiency for MET > 200 using Ele23: " << eff_met200_ele23 << " +" << (eff_UP_met200_ele23 - eff_met200_ele23)
-	    << " -" << (eff_met200_ele23 - eff_DN_met200_ele23) << std::endl;
+  // TEfficiency* h_htmet_eff_ht800 = new TEfficiency(*h_htmet_num_ht800, *h_met_denom_ht800);
+  // h_htmet_eff_ht800->SetLineColor(kGreen+2);
+  // h_htmet_eff_ht800->SetMarkerColor(kGreen+2);
+
+  TEfficiency* h_metor_eff_ele23 = new TEfficiency(*h_metor_num_ele23, *h_met_denom_ele23);
+  h_metor_eff_ele23->SetLineColor(kBlue);
+  h_metor_eff_ele23->SetMarkerColor(kBlue);
+
+  TEfficiency* h_met120_eff_ele23 = new TEfficiency(*h_met120_num_ele23, *h_met_denom_ele23);
+  h_met120_eff_ele23->SetLineColor(kRed);
+  h_met120_eff_ele23->SetMarkerColor(kRed);
+
+  TEfficiency* h_htmet_eff_ele23 = new TEfficiency(*h_htmet_num_ele23, *h_met_denom_ele23);
+  h_htmet_eff_ele23->SetLineColor(kGreen+2);
+  h_htmet_eff_ele23->SetMarkerColor(kGreen+2);
+
+  int bin_met250 = h_met_denom_ele23->FindBin(250.);
+  float num_met250 = h_metor_num_ele23->Integral(bin_met250,-1);
+  float denom_met250 = h_met_denom_ele23->Integral(bin_met250,-1);
+  float eff_met250_ele23 = num_met250/denom_met250;
+  float eff_UP_met250_ele23 = TEfficiency::ClopperPearson(denom_met250, num_met250, 0.68, 1);
+  float eff_DN_met250_ele23 = TEfficiency::ClopperPearson(denom_met250, num_met250, 0.68, 0);
+
+  std::cout << "efficiency for MET > 250 using Ele23: " << eff_met250_ele23 << " +" << (eff_UP_met250_ele23 - eff_met250_ele23)
+  	    << " -" << (eff_met250_ele23 - eff_DN_met250_ele23) << std::endl;
   
   //h_met_eff_ht800->Draw("pe same");
   //h_met_eff_mu20->Draw("pe same");
-  h_met_eff_ele23->Draw("pe same");
+  // h_met120_eff_ht800->Draw("pe same");
+  // h_htmet_eff_ht800->Draw("pe same");
+  // h_metor_eff_ht800->Draw("pe same");
+  h_met120_eff_ele23->Draw("pe same");
+  h_htmet_eff_ele23->Draw("pe same");
+  h_metor_eff_ele23->Draw("pe same");
 
-  TLegend* leg = new TLegend(0.55,0.15,0.95,0.45);
-  //leg->AddEntry(h_met_eff_ht800,"HLT_PFHT800","pe");
-  //leg->AddEntry(h_met_eff_mu20,"HLT_IsoMu20 || HLT_IsoTkMu20","pe");
-  leg->AddEntry(h_met_eff_ele23,"HLT_Ele23 || HLT_Ele27","pe");
+  TLegend* leg = new TLegend(0.35,0.15,0.95,0.45);
+  leg->SetHeader("Ref: Ele23 || Ele27_eta2p1 || Ele32_eta2p1");
+  // leg->AddEntry(h_metor_eff_ht800,"OR of PFMET trigs","pe");
+  // leg->AddEntry(h_htmet_eff_ht800,"PFHT300_PFMET110","pe");
+  // leg->AddEntry(h_met120_eff_ht800,"PFMET120_PFMHT120_IDTight","pe");
+  leg->AddEntry(h_metor_eff_ele23,"OR of PFMET trigs","pe");
+  leg->AddEntry(h_htmet_eff_ele23,"PFHT300_PFMET110","pe");
+  leg->AddEntry(h_met120_eff_ele23,"PFMET120_PFMHT120_IDTight","pe");
   leg->Draw("same");
 
   CMS_lumi( c, iPeriod, iPos );
--- scripts/trigeff/plot_trigeff_Photon165.C
+++ scripts/trigeff/plot_trigeff_Photon165.C
@@ -23,13 +23,13 @@ const int iPeriod = 4; // 13 tev
 //   iPos = 10*(alignement 1/2/3) + position (1/2/3 = left/center/right)
 const int iPos = 3;
 
-void plot_trigeff_Photon165 (const TString& indir = "/nfs-6/userdata/mt2/V00-08-02_json_Cert_271036-274421/") {
+void plot_trigeff_Photon165 (const TString& indir = "/nfs-6/userdata/mt2/V00-08-12_json_271036-284044_23Sep2016ReReco_36p26fb/trigdenom") {
 
   cmsText = "CMS Preliminary";
   cmsTextSize = 0.5;
   lumiTextSize = 0.4;
   writeExtraText = false;
-  lumi_13TeV = "2.1 fb^{-1}";
+  lumi_13TeV = "36.5 fb^{-1}";
   //lumi_13TeV = "589 pb^{-1}";
   
   gStyle->SetPadTopMargin(0.08);
@@ -40,7 +40,7 @@ void plot_trigeff_Photon165 (const TString& indir = "/nfs-6/userdata/mt2/V00-08-
   TH1::SetDefaultSumw2();
 
   //TString suffix = "_hovere001";
-  TString suffix = "_zmet2016";
+  TString suffix = "_36p5fb";
   //  TString suffix = "";
   
   TChain* t_jetht = new TChain("mt2");
@@ -68,13 +68,13 @@ void plot_trigeff_Photon165 (const TString& indir = "/nfs-6/userdata/mt2/V00-08-
   c->SetGrid(1,1);
   c->cd();
 
-  TCut base = "nVert > 0 && Flag_CSCTightHalo2015Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && ngamma > 0 && gamma_idCutBased[0] > 0 && gamma_chHadIso[0] < 2.5 ";
+  TCut base = "nVert > 0 && Flag_CSCTightHalo2015Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && isGolden && ngamma > 0 && gamma_idCutBased[0] > 0 && gamma_chHadIso[0] < 2.5 && gamma_hOverE015[0] < 0.1 ";
   //  TCut base = "nVert > 0 && Flag_CSCTightHalo2015Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && ngamma > 0 && gamma_idCutBased[0] > 0 && gamma_chHadIso[0] < 2.5 && gamma_hOverE[0] < 0.01 ";
   // -- cuts for ZMET 2016, or as close as I can get..
   //  TCut base = "nVert > 0 && Flag_CSCTightHalo2015Filter && Flag_eeBadScFilter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_goodVertices && nElectrons10 == 0 && ngamma > 0 && gamma_idCutBased[0] > 0 && ( (fabs(gamma_eta[0]) < 1.442 && gamma_chHadIso[0] < 3.32 && (gamma_neuHadIso[0] < 1.92 + 0.014  * gamma_pt[0] + 0.000019*pow(gamma_pt[0],2)) && gamma_phIso[0] < 0.81 + 0.0053 * gamma_pt[0]) || ((fabs(gamma_eta[0]) > 1.566 && fabs(gamma_eta[0]) < 2.4 && gamma_chHadIso[0] < 1.97 && (gamma_neuHadIso[0] < 11.86 + 0.0139  * gamma_pt[0] + 0.000025*pow(gamma_pt[0],2)) && gamma_phIso[0] < 0.83 + 0.0034 * gamma_pt[0]) ) ) ";
   TCut eb = "abs(gamma_eta[0]) < 1.479";
-  TCut ee = "abs(gamma_eta[0]) > 1.479 && abs(gamma_eta[0]) < 2.5";
-  TCut had = base + "nJet30 > 0";
+  TCut ee = "abs(gamma_eta[0]) > 1.479 && abs(gamma_eta[0]) < 2.4";
+  TCut had = base + "nJet30 > 0 && gamma_ht > 1000. && (HLT_PFHT900 || HLT_PFJet450)";
   //  TCut had = base + "ht > 900";
   TCut muon = base + "nMuons10 > 0 && abs(lep_pdgId[0]) == 13 && lep_pt[0] > 25.";
 
@@ -92,23 +92,23 @@ void plot_trigeff_Photon165 (const TString& indir = "/nfs-6/userdata/mt2/V00-08-
   
   TEfficiency* h_pt_eff_jetht = new TEfficiency(*h_pt_num_jetht, *h_pt_denom_jetht);
   h_pt_eff_jetht->SetName("h_pt_eff_jetht");
-  h_pt_eff_jetht->SetLineColor(kRed);
-  h_pt_eff_jetht->SetMarkerColor(kRed);
+  h_pt_eff_jetht->SetLineColor(kBlue);
+  h_pt_eff_jetht->SetMarkerColor(kBlue);
 
   TEfficiency* h_pt_eb_eff_jetht = new TEfficiency(*h_pt_eb_num_jetht, *h_pt_eb_denom_jetht);
   h_pt_eb_eff_jetht->SetName("h_pt_eb_eff_jetht");
-  h_pt_eb_eff_jetht->SetLineColor(kRed);
-  h_pt_eb_eff_jetht->SetMarkerColor(kRed);
+  h_pt_eb_eff_jetht->SetLineColor(kBlue);
+  h_pt_eb_eff_jetht->SetMarkerColor(kBlue);
 
   TEfficiency* h_pt_ee_eff_jetht = new TEfficiency(*h_pt_ee_num_jetht, *h_pt_ee_denom_jetht);
   h_pt_ee_eff_jetht->SetName("h_pt_ee_eff_jetht");
-  h_pt_ee_eff_jetht->SetLineColor(kRed);
-  h_pt_ee_eff_jetht->SetMarkerColor(kRed);
+  h_pt_ee_eff_jetht->SetLineColor(kGreen+3);
+  h_pt_ee_eff_jetht->SetMarkerColor(kGreen+3);
 
   h_pt_eff_jetht->Draw("pe same");
 
   TLegend* leg = new TLegend(0.6,0.2,0.9,0.5);
-  leg->AddEntry(h_pt_eff_jetht,"JetHT PD","pe");
+  leg->AddEntry(h_pt_eff_jetht,"PFHT900 || PFJet450","pe");
   leg->Draw("same");
 
   CMS_lumi( c, iPeriod, iPos );
@@ -124,35 +124,37 @@ void plot_trigeff_Photon165 (const TString& indir = "/nfs-6/userdata/mt2/V00-08-
 
   h_axis->Draw();
   h_pt_eb_eff_jetht->Draw("pe same");
+  h_pt_ee_eff_jetht->Draw("pe same");
   
   TLegend* leg_eb = new TLegend(0.6,0.2,0.9,0.5);
-  leg_eb->SetHeader("EB only");
-  leg_eb->AddEntry(h_pt_eff_jetht,"JetHT PD","pe");
+  leg_eb->SetHeader("Ref: PFHT900 || PFJet450");
+  leg_eb->AddEntry(h_pt_eb_eff_jetht,"EB Photons","pe");
+  leg_eb->AddEntry(h_pt_ee_eff_jetht,"EE Photons","pe");
   leg_eb->Draw("same");
 
   CMS_lumi( c_eb, iPeriod, iPos );
-  c_eb->SaveAs(Form("trigeff_Photon165_EB%s.pdf",suffix.Data()));
-  c_eb->SaveAs(Form("trigeff_Photon165_EB%s.eps",suffix.Data()));
-  c_eb->SaveAs(Form("trigeff_Photon165_EB%s.png",suffix.Data()));
+  c_eb->SaveAs(Form("trigeff_Photon165_EBEE%s.pdf",suffix.Data()));
+  c_eb->SaveAs(Form("trigeff_Photon165_EBEE%s.eps",suffix.Data()));
+  c_eb->SaveAs(Form("trigeff_Photon165_EBEE%s.png",suffix.Data()));
 
-  // ----- EE -----------
+  // // ----- EE -----------
 
-  TCanvas* c_ee = new TCanvas("c_ee","c_ee");
-  c_ee->SetGrid(1,1);
-  c_ee->cd();
+  // TCanvas* c_ee = new TCanvas("c_ee","c_ee");
+  // c_ee->SetGrid(1,1);
+  // c_ee->cd();
 
-  h_axis->Draw();
-  h_pt_ee_eff_jetht->Draw("pe same");
+  // h_axis->Draw();
+  // h_pt_ee_eff_jetht->Draw("pe same");
   
-  TLegend* leg_ee = new TLegend(0.6,0.2,0.9,0.5);
-  leg_ee->SetHeader("EE only");
-  leg_ee->AddEntry(h_pt_eff_jetht,"JetHT PD","pe");
-  leg_ee->Draw("same");
+  // TLegend* leg_ee = new TLegend(0.6,0.2,0.9,0.5);
+  // leg_ee->SetHeader("EE only");
+  // leg_ee->AddEntry(h_pt_eff_jetht,"PFHT900 || PFJet450","pe");
+  // leg_ee->Draw("same");
 
-  CMS_lumi( c_ee, iPeriod, iPos );
-  c_ee->SaveAs(Form("trigeff_Photon165_EE%s.pdf",suffix.Data()));
-  c_ee->SaveAs(Form("trigeff_Photon165_EE%s.eps",suffix.Data()));
-  c_ee->SaveAs(Form("trigeff_Photon165_EE%s.png",suffix.Data()));
+  // CMS_lumi( c_ee, iPeriod, iPos );
+  // c_ee->SaveAs(Form("trigeff_Photon165_EE%s.pdf",suffix.Data()));
+  // c_ee->SaveAs(Form("trigeff_Photon165_EE%s.eps",suffix.Data()));
+  // c_ee->SaveAs(Form("trigeff_Photon165_EE%s.png",suffix.Data()));
 
   f_out->Write();
   h_pt_eff_jetht->Write();
