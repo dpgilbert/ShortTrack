234cf73ba7966961dbd8c1f2262d292463b9f391
fgolf <frank.golf@gmail.com>
2017-01-08 20:06:50 -0800

    check whether or not to merge top samples

--- RebalanceAndSmear/plotting/looper_output/merge_all.py
+++ RebalanceAndSmear/plotting/looper_output/merge_all.py
@@ -2,9 +2,11 @@ import sys
 import os
 from merge import merge
 
-#dirs = ['data', 'data_noRS', 'qcd_noRS', 'singletop_noRS', 'ttdl_noRS', 'ttsl_noRS', 'ttw_noRS', 'ttz_noRS', 'wjets_noRS', 'zinv_noRS']
-dirs = ['qcd_noRS', 'singletop_noRS', 'ttdl_noRS', 'ttsl_noRS', 'ttw_noRS', 'ttz_noRS', 'wjets_noRS', 'zinv_noRS']
+dirs = ['data', 'data_noRS', 'qcd', 'qcd_noRS', 'singletop_noRS', 'ttdl_noRS', 'ttsl_noRS', 'ttw_noRS', 'ttz_noRS', 'wjets_noRS', 'zinv_noRS']
+#dirs = ['qcd', 'qcd_noRS', 'singletop_noRS', 'ttdl_noRS', 'ttsl_noRS', 'ttw_noRS', 'ttz_noRS', 'wjets_noRS', 'zinv_noRS']
+#dirs = ['data_noRS']
 tops = ['singletop_noRS', 'ttdl_noRS', 'ttsl_noRS', 'ttw_noRS', 'ttz_noRS']
+#tops = []
 
 basedir = sys.argv[1]
 test = sys.argv[2] if len(sys.argv) > 2 else False
@@ -13,14 +15,15 @@ for dirname in dirs:
   if "ttdl" not in dirname:
     merge(os.path.join(basedir,dirname), test)
   if "wjets" in dirname or "zinv" in dirname:
-    print "cp {0}/{1}/merged_hists.root {2}/{3}".format(basedir, dirname, basedir, dirname.replace('noRS','ht'))
+    print "cp {0}/{1}/merged_hists.root {2}/{3}".format(basedir, dirname, basedir, dirname.replace('noRS','ht.root'))
     if not test:
-      os.system("cp {0}/{1}/merged_hists.root {2}/{3}".format(basedir, dirname, basedir, dirname.replace('noRS','ht')))
+      os.system("cp {0}/{1}/merged_hists.root {2}/{3}".format(basedir, dirname, basedir, dirname.replace('noRS','ht.root')))
 
 hadd_input = " "
 for top in tops:
     hadd_input += "{0}/{1}/merged_hists.root ".format(basedir,top)
 
-print "hadd " + os.path.join(basedir,"top.root") + " " + hadd_input
-if not test:
-  os.system("hadd " + os.path.join(basedir,"top.root") + " " + hadd_input)
+if len(tops) > 0:
+  print "hadd " + os.path.join(basedir,"top.root") + " " + hadd_input
+  if not test:
+    os.system("hadd " + os.path.join(basedir,"top.root") + " " + hadd_input)
