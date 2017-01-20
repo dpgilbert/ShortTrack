24379d27f5a679dc716ea09d60de524a87c175d8
gzevi <giovanni.zevidellaporta@gmail.com>
2017-01-20 12:21:46 -0800

    simplify synching with different folder structures

--- scripts/sync/compareCRyields_sntroot.py
+++ scripts/sync/compareCRyields_sntroot.py
@@ -13,7 +13,10 @@ import ROOT, math
 #snt_file = ROOT.TFile("../../MT2looper/output/V00-08-12_json_271036-284044_23Sep2016ReReco_36p46fb_dypt100_30/lostlepFromCRs.root")
 #snt_file = ROOT.TFile("../../MT2looper/output/V00-08-12_json_271036-284044_23Sep2016ReReco_36p46fb_dypt100_30/zinvFromDY.root")
 #snt_file = ROOT.TFile("../../MT2looper/output/V00-08-12_json_271036-284044_23Sep2016ReReco_36p46fb_dypt100_30_nottbarisrweight/lostlepFromCRs.root")
-snt_file = ROOT.TFile("../../MT2looper/output/V00-08-12_json_271036-284044_23Sep2016ReReco_36p46fb_dy7j1bmerge/zinvFromDY.root")
+#snt_file = ROOT.TFile("../../MT2looper/output/V00-08-12_json_271036-284044_23Sep2016ReReco_36p46fb_dy7j1bmerge/zinvFromDY.root")'
+#snt_file = ROOT.TFile("../../MT2looper/output/V00-08-09_7Dec16/zinvFromDY.root")
+#snt_file = ROOT.TFile("../../MT2looper/output/V00-08-09_7Dec16/lostlepFromCRs.root")
+#snt_file = ROOT.TFile("../../MT2looper/output/V00-08-09_5Dec16/data_Run2016.root")
 
 #eth_file = ROOT.TFile("eth_estimates/gamma_data.root")
 #eth_file = ROOT.TFile("zinvFromGamma_80X.root")
@@ -34,21 +37,27 @@ snt_file = ROOT.TFile("../../MT2looper/output/V00-08-12_json_271036-284044_23Sep
 #eth_file = ROOT.TFile("eth_sync_dec2016/llepEstimate_36p5ifb.root")
 #eth_file = ROOT.TFile("eth_sync_dec2016/zinvFromZll_36p5ifb.root")
 #eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/zinvFromZll.root")
-eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/zinvFromZll_updatedShape.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/zinvFromZll_updatedShape.root")
 #eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/llepEstimate.root")
 #eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/llepEstimate_fixedISR.root")
 #eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/llepEstimate_fixed.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/data_forZinvEst.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/data_forZinvEst_08Dec.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/data_of_forZinvEst.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/zinvFromZll_noBtagSF_new.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/zinvFromZll_36p5ifb_08Dec.root")
+#eth_file = ROOT.TFile("eth_sync_dec2016_snttrees/llepEstimate_noBtagSF.root")
 
 #snt_label = "crsl"
 #snt_label = "crdy"
 #snt_label = "crgj"
-snt_label = "sr"
+#snt_label = "sr"
 #eth_label = "gammaCR" 
 #eth_label = "Zinv"
 #eth_label = "llepCR"
 #eth_label = "llepEstimate"
 #eth_label = "llep_est_integral"
-eth_label = "ZinvEstimateFromZll_hybrid"
+#eth_label = "ZinvEstimateFromZll_hybrid"
 #eth_label = "zllData_forHybrid"
 #eth_label = "data"
 #eth_label = "data_of"
@@ -58,6 +67,49 @@ eth_label = "ZinvEstimateFromZll_hybrid"
 
 #snt_label = "sr"
 #eth_label = "Zinv"
+
+
+# Working combos:
+snt_folder = "../../MT2looper/output/full2016unblind/"
+eth_folder = "20Jan16eth"
+#type = "data"
+#type = "ZinvEst"
+#type = "LLepEst"
+#type = "LLepCR"
+type = "ZinvCR"
+
+print "Looking at: " + type
+
+if (type == "ZinvEst") :
+    snt_file = ROOT.TFile(snt_folder+"/zinvFromDY.root")
+    eth_file = ROOT.TFile(eth_folder+"/zinvFromZll_Jan20.root")
+    snt_label = "sr"
+    eth_label = "ZinvEstimateFromZll_hybrid"
+
+if (type == "data") :
+    snt_file = ROOT.TFile(snt_folder+"/data_Run2016.root")
+    eth_file = ROOT.TFile(eth_folder+"/dataSR_Jan20.root")
+    snt_label = "sr"
+    eth_label = "data"
+
+if (type == "LLepEst") :
+    snt_file = ROOT.TFile(snt_folder+"/lostlepFromCRs.root")
+    eth_file = ROOT.TFile(eth_folder+"/llepEstimate_Jan20.root")
+    snt_label = "sr"
+    eth_label = "llepEstimate"
+
+if (type == "LLepCR") :
+    snt_file = ROOT.TFile(snt_folder+"/data_Run2016.root")
+    eth_file = ROOT.TFile(eth_folder+"/llepCR_Jan20.root")
+    snt_label = "crsl"
+    eth_label = "llepCR"
+
+if (type == "ZinvCR") :
+    snt_file = ROOT.TFile(snt_folder+"/data_Run2016.root")
+    eth_file = ROOT.TFile(eth_folder+"/zllCR_Jan20.root")
+    snt_label = "crdy"
+    eth_label = "data"
+
 
 eth_yields = {}
 snt_yields = {}
@@ -93,20 +145,34 @@ for ht_reg in ["VL","L","M","H","UH"]:
         if top_reg in [10,11,15]: b_name = "b3toInf"
 
         ethname = "{0}_{1}_{2}".format(ht_name,j_name,b_name)
+        #print eth_label
+        #print ethname
         try:
 #            sntyield = int(snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))   # int
 #            sntyield = (snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))    # float
-            sntyield = (snt_file.Get("{0}/hybridEstimate".format(sntname)).Integral(0,-1))    # float
+#            sntyield = (snt_file.Get("{0}/hybridEstimate".format(sntname)).Integral(0,-1))    # float
+#            sntyield = (snt_file.Get("{0}/CRyieldCard".format(sntname)).Integral(0,1))    # float
+#            sntyield = (snt_file.Get("{0}/h_mt2binsCRyieldEM".format(sntname)).Integral(0,-1))    # float
 #            sntyield = (snt_file.Get("{0}/h_mt2binsemu".format(sntname)).Integral(0,-1))    # float
 #            sntyield = int(snt_file.Get("{0}/h_mt2bins".format(sntname)).GetEntries())    # int, entries
+            if (type == "data" or type == "LLepCR" or type == "ZinvCR"):
+                sntyield = int(snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))   # int
+            elif (type == "ZinvEst" or type == "LLepEst"):
+                sntyield = (snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))    # float
         except:
             sntyield = 0
 #        ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral(1,-1))   # int
 #        ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral(1,1))   # int, first bin only
 #        print "{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)
-        ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral(1,-1))   # float
+#        ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral())   # float
 #        ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral(1,1))   # float, first bin only
 #        ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).GetEntries())   # int, entries
+        
+        if (type == "data" or type == "LLepCR" or type == "ZinvCR"):
+            ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral(1,-1))   # int
+        elif (type == "ZinvEst" or type == "LLepEst"):
+            ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral(1,-1))   # float
+
 
         snt_yields[ethname] = sntyield
         eth_yields[ethname] = ethyield
@@ -131,19 +197,35 @@ for mj_reg in range(1,8)+range(11,16):
     ethname = "{0}_{1}".format(ht_name,jbj_name)
 
     try:
+       # print eth_label
+       # print ethname
 #    sntyield = int(snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))                           # int
 #        sntyield = (snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))                               # float
-        sntyield = (snt_file.Get("{0}/hybridEstimate".format(sntname)).Integral(0,-1))                               # float
+#        sntyield = (snt_file.Get("{0}/hybridEstimate".format(sntname)).Integral(0,-1))                               # float
+#        sntyield = (snt_file.Get("{0}/CRyieldCard".format(sntname)).Integral(0,1))                               # float
+#        sntyield = (snt_file.Get("{0}/h_mt2binsCRyieldEM".format(sntname)).Integral(0,-1))                               # float
 #        sntyield = (snt_file.Get("{0}/h_mt2binsemu".format(sntname)).Integral(0,-1))                               # float
 #    sntyield = int(snt_file.Get("{0}/h_mt2bins".format(sntname)).GetEntries())                             # int, entries
+        if (type == "data" or type == "LLepCR" or type == "ZinvCR"):
+            sntyield = int(snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))   # int
+        elif (type == "ZinvEst" or type == "LLepEst"):
+            sntyield = (snt_file.Get("{0}/h_mt2bins".format(sntname)).Integral(0,-1))    # float
     except:
         sntyield = 0.
 #    ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral(1,-1))         # int    
 #    ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral(1,1))         # int, first bin only    
-    ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral(1,-1))             # float
-#    ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral(1,1))             # float, first bin only
+#    ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral(1,-1))             # float
+#    ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral())             # float, first bin only
 #    ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).GetEntries())             # int, entries, first bin only
     
+
+    if (type == "data" or type == "LLepCR" or type == "ZinvCR"):
+        ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label, ethname)).Integral())   # int
+        #ethyield = int(eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).GetEntries())             # int, entries, first bin only
+
+    elif (type == "ZinvEst" or type == "LLepEst"):
+        ethyield = (eth_file.Get("{0}/{1}/yield_{0}_{1}".format(eth_label,ethname)).Integral(1,-1))             # float
+
     snt_yields[ethname] = sntyield
     eth_yields[ethname] = ethyield
     if not math.isnan(sntyield): snt_total += sntyield
@@ -164,11 +246,13 @@ for key in eth_yields.keys():
 
 ratios_sorted = sorted(ratios.items(), key=lambda x: x[1], reverse=True)
 
-print "{0:30s} {1:>14s} {2:>14s} {3:>8s}".format("Region", "SNT CR yield", "ETH CR yield", "Ratio")
+print "{0:30s} {1:>14s} {2:>14s} {3:>8s}".format("Region", "SNT yield", "ETH yield", "Ratio")
 print "--------------------------------------------------------------------------------"
 for key,r in ratios_sorted: 
-#    print "{0:30s} {1:14d} {2:14d} {3:8.5f}".format(key,snt_yields[key],eth_yields[key],r)         # int
-    print "{0:30s} {1:14f} {2:14f} {3:8.5f}".format(key,snt_yields[key],eth_yields[key],r)          # float
+    if (type == "data"):
+        print "{0:30s} {1:14d} {2:14d} {3:8.5f}".format(key,snt_yields[key],eth_yields[key],r)         # int
+    else:
+        print "{0:30s} {1:14f} {2:14f} {3:8.5f}".format(key,snt_yields[key],eth_yields[key],r)         # float
 
 print "--------------------------------------------------------------------------------"
 print "{0:30s} {1:14f} {2:14f} {3:8.5f}".format("total events",snt_total,eth_total,snt_total/eth_total)
