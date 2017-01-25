65d538cabe3f26d9906ba335ec9c112396cd2f59
olivito <dominick.olivito@gmail.com>
2017-01-25 11:02:32 +0100

    script to copy CR plots to AN

---
+++ scripts/CRplotMaker/copy_to_AN.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+PLOTDIR=./crplots/crplots_36p46fb_V15/
+ANDIRZINV=/home/olivito/latex/cmsnotes/tdr2/notes/AN-16-460/trunk/Figures/zinv/Moriond17/
+ANDIRLOSTLEP=/home/olivito/latex/cmsnotes/tdr2/notes/AN-16-460/trunk/Figures/lostlep/rereco_36p5fb/
+
+cp $PLOTDIR/crdybase/crdybase_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crdybase/crdybase_mt2.pdf $ANDIRZINV
+cp $PLOTDIR/crdybase/crdybase_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crdybase/crdybase_nJet30.pdf $ANDIRZINV
+cp $PLOTDIR/crdybaseIncl/crdybaseIncl_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crdybaseIncl/crdybaseIncl_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crdybaseIncl/crdybaseIncl_nJet30.pdf $ANDIRZINV
+cp $PLOTDIR/crdybaseJ/crdybaseJ_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crdybaseJ/crdybaseJ_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbase/crgjbase_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbase/crgjbase_mt2.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbase/crgjbase_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbase/crgjbase_nJet30.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbaseIncl/crgjbaseIncl_gammaPt.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbaseIncl/crgjbaseIncl_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbaseIncl/crgjbaseIncl_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbaseIncl/crgjbaseIncl_nJet30.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbaseJ/crgjbaseJ_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crgjbaseJ/crgjbaseJ_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbase/crrlbase_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbase/crrlbase_mt2.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbase/crrlbase_nBJet20.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbase/crrlbase_nJet30.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbaseIncl/crrlbaseIncl_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbaseIncl/crrlbaseIncl_nJet30.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbaseJ/crrlbaseJ_ht.pdf $ANDIRZINV
+cp $PLOTDIR/crrlbaseJ/crrlbaseJ_nBJet20.pdf $ANDIRZINV
+
+cp $PLOTDIR/crslbase/crslbase_ht.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbase/crslbase_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbase/crslbase_nBJet20.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbase/crslbase_nJet30.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbaseH/crslbaseH_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbaseJ/crslbaseJ_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbaseL/crslbaseL_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbaseM/crslbaseM_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbaseUH/crslbaseUH_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslbaseVL/crslbaseVL_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslelbase/crslelbase_ht.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslelbase/crslelbase_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslelbase/crslelbase_nBJet20.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslelbase/crslelbase_nJet30.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslmubase/crslmubase_ht.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslmubase/crslmubase_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslmubase/crslmubase_nBJet20.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslmubase/crslmubase_nJet30.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslttbar/crslttbar_ht.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslttbar/crslttbar_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslttbar/crslttbar_nJet30.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslwjets/crslwjets_ht.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslwjets/crslwjets_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/crslwjets/crslwjets_nJet30.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_0b_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_ge1b_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_incl_HT1000to1500_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_incl_HT250to450_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_incl_HT450to575_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_incl_HT575to1000_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_incl_HTge1500_mt2bins.pdf $ANDIRLOSTLEP
+cp $PLOTDIR/lostlepHybrid/lostlepHybrid_incl_mt2bins.pdf $ANDIRLOSTLEP
