dd5d88f1bc929ebd2b560a30e54064d04af1781a
gzevi <giovanni.zevidellaporta@gmail.com>
2017-02-07 17:36:47 +0100

    added macroregion plots

--- scripts/ResultPlotMaker/do.py
+++ scripts/ResultPlotMaker/do.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 import ROOT
-from ResultPlotMaker import MakePlot, MakeComparison
+from ResultPlotMaker import MakePlot, MakeComparison, MakeMacroRegionPlot
 import ResultPlotUtils as utils
 
 datacard_dir = "/home/users/mderdzinski/winter2017/clean_master/scripts/cards_T2bb_Feb1"
+#datacard_dir = "../cards_test"
 
 # {0} is the HT region, {1} is the jet/bjet region, {2} is the mt2 bin
 datacard_name = "datacard_{0}_{1}_{2}_T2bb_375_425.txt"
+#datacard_name = "datacard_{0}_{1}_{2}_ttw_mg_lo.txt"
 
 outdir = "plots"
 
@@ -26,3 +28,13 @@ MakePlot("monojet",datacard_dir, datacard_name, outdir, userMax=7e7)
 # MakeComparison("HT200to450", datacard_dir, datacard_dir_eth, datacard_name, datacard_name_eth, outdir, iBkgd=2, userMax=7e5)
 # MakeComparison("HT575to1000", datacard_dir, datacard_dir_eth, datacard_name, datacard_name_eth, outdir, iBkgd=1, userMax=7e5)
 # MakeComparison("HT575to1000", datacard_dir, datacard_dir_eth, datacard_name, datacard_name_eth, outdir, iBkgd=2, userMax=7e5)
+
+#datacard_name_topo = "datacard_{0}_{1}_ttw_mg_lo.txt"
+#MakeMacroRegionPlot(1,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(2,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(3,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(4,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(5,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(6,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(7,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
+#MakeMacroRegionPlot(8,datacard_dir, datacard_name_topo, outdir, userMax=5e6)
