6037d1e2e46684053c2fb543406d287185cb15dd
mderdzinski <mark.derdzinski@gmail.com>
2017-02-15 07:50:59 -0800

    update most scripts to most recent babies, merge lists

--- MT2looper/do.sh
+++ MT2looper/do.sh
@@ -3,25 +3,35 @@
 make -j12
 if [[ $? != 0 ]]; then return $?; fi
 
-OUTDIR=output/full2016
+OUTDIR=output/full2016_v16_Feb15
 LOGDIR=logs/
 mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
 
 #most recent MC
-INDIR=/nfs-6/userdata/mt2/V00-08-10_JECandRenorm_nojson_skim_base_mt2gt200_ZinvV6/
-declare -a Samples=(ww ttsl ttdl singletop qcd_ht ttw_mg_lo ttz_mg_lo ttg dyjetsll_ht gjets_dr0p05_ht wjets_ht100to200 wjets_ht1200to2500 wjets_ht200to400 wjets_ht2500toInf wjets_ht400to600 wjets_ht600to800 wjets_ht800to1200 zinv_ht)
+INDIR=/nfs-6/userdata/mt2/V00-08-16_skim_base_mt2gt200_ZinvV6_JECs/
+declare -a Samples=(ttsl ttdl singletop qcd_ht ttw_mg_lo ttz_mg_lo ttg tth dyjetsll_ht gjets_dr0p05_ht wjets_ht100to200 wjets_ht1200to2500 wjets_ht200to400 wjets_ht2500toInf wjets_ht400to600 wjets_ht600to800 wjets_ht800to1200 zinv_ht)
+#declare -a Samples=(wz_1l3n wz_3lnu wz_lnqq ww_2l2nu ww_lnuqq www_incl_amcnlo wwz_incl_amcnlo wzz_incl_amcnlo zzz_incl_amcnlo zz_2l2n zz_2l2q zz_2q2n zz_4l) #diboson and triboson samples
 
 for SAMPLE in ${Samples[@]};
-  do echo ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
-     nohup ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
+  do echo nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
+     nohup nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
+done
+
+#signal
+INDIR=/nfs-6/userdata/mt2/V00-08-16_skim_base_mt2gt200_ZinvV6_JECs/
+declare -a Samples=(T1bbbb T1qqqq T1tttt T2bb T2qq T2tt)
+
+for SAMPLE in ${Samples[@]};
+  do echo nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
+     nohup nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
 done
 
 #most recent run2016H and re-reco
-INDIR=/nfs-6/userdata/mt2/V00-08-12_json_271036-284044_23Sep2016ReReco_36p26fb_skim_base_mt2gt200_ZinvV6/
+INDIR=/nfs-6/userdata/mt2/V00-08-16_skim_base_mt2gt200_ZinvV6_JECs/
 declare -a Samples=(data_Run2016B data_Run2016C data_Run2016D data_Run2016E data_Run2016F data_Run2016G data_Run2016H)
 
 for SAMPLE in ${Samples[@]};
-  do echo ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
-  nohup ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
+  do echo nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR}
+  nohup nice -n 10 ./runLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &
 done
--- scripts/makeDataDrivenEstimates.sh
+++ scripts/makeDataDrivenEstimates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INDIR=/home/users/fgolf/mt2/devel/MT2Analysis/MT2looper/output/full2016/
+INDIR=/home/users/mderdzinski/winter2017/clean_master/MT2looper/output/full2016_v16_Feb15/
 
 THISDIR=`pwd`
 
@@ -30,17 +30,38 @@ cd $INDIR
 echo "hadd -f data_Run2016.root data_Run2016B.root data_Run2016C.root data_Run2016D.root data_Run2016E.root data_Run2016F.root data_Run2016G.root data_Run2016H.root"
 hadd -f data_Run2016.root data_Run2016B.root data_Run2016C.root data_Run2016D.root data_Run2016E.root data_Run2016F.root data_Run2016G.root data_Run2016H.root > dataDrivenEstimates.log
 
-## combine wjets_ht bins
+# combine wjets_ht bins
 echo "hadd -f wjets_ht.root wjets_ht100to200.root wjets_ht200to400.root wjets_ht400to600.root wjets_ht600to800.root wjets_ht800to1200.root wjets_ht1200to2500.root wjets_ht2500toInf.root"
 hadd -f wjets_ht.root wjets_ht100to200.root wjets_ht200to400.root wjets_ht400to600.root wjets_ht600to800.root wjets_ht800to1200.root wjets_ht1200to2500.root wjets_ht2500toInf.root >> dataDrivenEstimates.log
 
-## (temporarily replaced tth with ttg, because of sample availability)
+# (temporarily replaced tth with ttg, because of sample availability)
 echo "hadd -f top.root ttsl.root ttdl.root singletop.root ttw.root ttz.root ttg.root"
 hadd -f top.root ttsl.root ttdl.root singletop.root ttw_mg_lo.root ttz_mg_lo.root ttg.root >> dataDrivenEstimates.log
 
-## make "lostlep" from sum of MC lostlep backgrounds (except QCD, to avoid spikes)
-echo "hadd -f lostlep.root ttsl.root ttdl.root singletop.root ttw.root ttz.root ttg.root wjets_ht.root" ## (temporarily replaced tth with ttg, because of sample availability)
-hadd -f lostlep.root ttsl.root ttdl.root singletop.root ttw_mg_lo.root ttz_mg_lo.root ttg.root wjets_ht.root >> dataDrivenEstimates.log
+# make "lostlep" from sum of MC lostlep backgrounds (except QCD, to avoid spikes)
+echo "hadd -f lostlep.root ttsl.root ttdl.root singletop.root ttw.root ttz.root tth.root wjets_ht.root"
+hadd -f lostlep.root ttsl.root ttdl.root singletop.root ttw_mg_lo.root ttz_mg_lo.root tth.root wjets_ht.root >> dataDrivenEstimates.log
+
+# echo "hadd -f wz.root wz_1l3n.root wz_3lnu.root wz_lnqq.root"
+# hadd -f wz.root wz_1l3n.root wz_3lnu.root wz_lnqq.root >> dataDrivenEstimates.log
+
+# echo "hadd -f ww.root ww_2l2nu.root ww_lnuqq.root"
+# hadd -f ww.root ww_2l2nu.root ww_lnuqq.root >> dataDrivenEstimates.log
+
+# echo "hadd -f zz.root zz_2l2n.root zz_2l2q.root zz_2q2n.root zz_4l.root"
+# hadd -f zz.root zz_2l2n.root zz_2l2q.root zz_2q2n.root zz_4l.root >> dataDrivenEstimates.log
+
+# echo "hadd -f vv.root ww_2l2nu.root ww_lnuqq.root zz_2l2n.root zz_2l2q.root zz_2q2n.root zz_4l.root"
+# hadd -f vv.root ww_2l2nu.root ww_lnuqq.root zz_2l2n.root zz_2l2q.root zz_2q2n.root zz_4l.root >> dataDrivenEstimates.log
+
+# echo "hadd -f vvv.root  www_incl_amcnlo.root wwz_incl_amcnlo.root wzz_incl_amcnlo.root zzz_incl_amcnlo.root"
+# hadd -f vvv.root  www_incl_amcnlo.root wwz_incl_amcnlo.root wzz_incl_amcnlo.root zzz_incl_amcnlo.root >> dataDrivenEstimates.log
+
+## make "lostlep" from sum of MC lostlep backgrounds and diboson (except QCD, to avoid spikes)
+# echo "hadd -f lostlep_diboson.root ttsl.root ttdl.root singletop.root ttw.root ttz.root ttg.root wjets_ht.root ww.root wz.root" ## (temporarily replaced tth with ttg, because of sample availability)
+# hadd -f lostlep_diboson.root ttsl.root ttdl.root singletop.root ttw_mg_lo.root ttz_mg_lo.root ttg.root wjets_ht.root ww.root wz.root >> dataDrivenEstimates.log
+
+
 cd $THISDIR
 
 #this script scales the HI and LOW boundary histograms by 1/numSamples since we don't want these hadd'ed
@@ -53,8 +74,26 @@ root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/wjets_ht.root\",7)" >> dataDriven
 echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/top.root,6)"
 root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/top.root\",6)" >> dataDrivenEstimates.log
 
+# echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/wz.root,3)"
+# root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/wz.root\",3)" >> dataDrivenEstimates.log
+
+# echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/ww.root,2)"
+# root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/ww.root\",2)" >> dataDrivenEstimates.log
+
+# echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/zz.root,4)"
+# root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/zz.root\",4)" >> dataDrivenEstimates.log
+
+# echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/vv.root,6)"
+# root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/vv.root\",6)" >> dataDrivenEstimates.log
+
+# echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/vvv.root,4)"
+# root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/vvv.root\",4)" >> dataDrivenEstimates.log
+
 echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/lostlep.root,13)"
 root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/lostlep.root\",13)" >> dataDrivenEstimates.log
+
+# echo "root -b -q rescaleBoundaryHists.C+(${INDIR}/lostlep_diboson.root,18)"
+# root -b -q "rescaleBoundaryHists.C+(\"${INDIR}/lostlep_diboson.root\",18)" >> dataDrivenEstimates.log
 
 # make the data driven background files
 echo "root -b -q lostlepMaker.C+(${INDIR},${LOSTLEPFILE})"
